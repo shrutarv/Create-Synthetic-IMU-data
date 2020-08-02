c0f60f4ee5b0c6b4591731d38d83af15f9e00cc0
shrutarv <shrutarv.awasthi@gmail.com>
2020-08-02 10:35:19 +0530

    Update op_sliding_window.py

--- op_sliding_window.py
+++ op_sliding_window.py
@@ -103,14 +103,14 @@ ws = (200,31)
 ss = (25,31)
 sliding_window_length = 200    
 sliding_window_step = 25
-data_dir =  "/data/sawasthi/data/testData/"
+data_dir =  "/data/sawasthi/data/trainData/"
 #data_dir = "/media/shrutarv/Drive1/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows2/"
 #data_dir = "S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows2/"
 #for i in sliding_window(data_y,(ws,data_y.shape[1]),(ss,1)):
     
 #    print (np.shape(i[:,0]))
 FileList_y = []
-os.chdir('/vol/actrec/DFG_Project/2019/Mbientlab/recordings_2019/07_IMU_synchronized_annotated/P13')
+os.chdir('/vol/actrec/DFG_Project/2019/Mbientlab/recordings_2019/07_IMU_synchronized_annotated/P07')
 #os.chdir("/media/shrutarv/Drive1/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/S13/")
 #os.chdir('S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/S13')
 FileList_y = glob.glob('*labels.csv')
@@ -120,7 +120,7 @@ FileList_y = glob.glob('*labels.csv')
         
 FileList_x = []
 #os.chdir('S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/S13')
-os.chdir('/vol/actrec/DFG_Project/2019/Mbientlab/recordings_2019/07_IMU_synchronized_annotated/P13')
+os.chdir('/vol/actrec/DFG_Project/2019/Mbientlab/recordings_2019/07_IMU_synchronized_annotated/P07')
 #os.chdir("/media/shrutarv/Drive1/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/S13/")
 FileList_x = glob.glob('*.csv')
 #os.chdir('S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/S14')
@@ -133,7 +133,7 @@ FileList_x = list(set_x - set_y)
 FileList_x.sort()
 FileList_y.sort()
 k = 0 
-folder_name = "P13"
+folder_name = "P07"
 for i,j in zip(FileList_x, FileList_y):
     k += 1
     data_y = pd.read_csv("/vol/actrec/DFG_Project/2019/Mbientlab/recordings_2019/07_IMU_synchronized_annotated/" + folder_name + "/" + j) 
