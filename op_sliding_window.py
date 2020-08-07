63d05845a9882a05ff40fb8e3232192826599ddd
shrutarv <shrutarv.awasthi@gmail.com>
2020-08-08 05:22:29 +0530

    Update op_sliding_window.py

--- op_sliding_window.py
+++ op_sliding_window.py
@@ -119,7 +119,7 @@ data_dir =  "/data/sawasthi/data/MoCAP_data/trainData/"
 #for i in sliding_window(data_y,(ws,data_y.shape[1]),(ss,1)):
 
 #    print (np.shape(i[:,0]))
-folder_name = "P06"
+folder_name = "P07"
 FileList_y = []
 os.chdir('/vol/actrec/DFG_Project/2019/Mbientlab/recordings_2019/07_IMU_synchronized_annotated/' + folder_name)
 #os.chdir("/vol/actrec/DFG_Project/2019/MoCap/recordings_2019/14_Annotated_Dataset/P06/" + folder_name)
