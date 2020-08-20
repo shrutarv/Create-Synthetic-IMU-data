17a21d6092768cd1f411d829009e0d7eeb1b7520
shrutarv <shrutarv.awasthi@gmail.com>
2020-08-21 04:45:12 +0530

    Update Main.py

--- MoCAP/Main.py
+++ MoCAP/Main.py
@@ -233,7 +233,7 @@ if __name__ == '__main__':
         data_x = harwindow_batched["data"]
         data_x.to(device)
         value = max_min_values(data_x,value)
-        '''
+    '''
     # Test data    
     path = '/data/sawasthi/data/MoCAP_data/testData/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
@@ -244,12 +244,12 @@ if __name__ == '__main__':
                                    num_workers=0,
                                    pin_memory=True,
                                    drop_last=True)
-        '''
+    '''
     for b, harwindow_batched in enumerate(dataLoader_test):
         data_x = harwindow_batched["data"]
         data_x.to(device)
         value = max_min_values(data_x,value)
-         '''
+    '''
     print('Start Training')
     correct = 0
     counter = 0 
