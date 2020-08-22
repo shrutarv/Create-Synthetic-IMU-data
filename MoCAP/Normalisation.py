eceefa2767d043ffbd63ff782c37883004051165
shrutarv <shrutarv.awasthi@gmail.com>
2020-08-22 17:23:28 +0530

    Update Normalisation.py

--- MoCAP/Normalisation.py
+++ MoCAP/Normalisation.py
@@ -14,7 +14,7 @@ def max_min_values(data, values):
     temp_values = []
     data = data.numpy()
     data = data.reshape(data.shape[0],data.shape[2], data.shape[3])
-    
+    print(data.shape)
     temp_values = []
     for attr in range(data.shape[2]):
         attribute = []
