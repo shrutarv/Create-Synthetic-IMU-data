e911f9ecf46f54ffd1f857ee28e3acd41f375a5f
shrutarv <shrutarv.awasthi@gmail.com>
2020-09-17 20:13:11 +0530

    Update Main.py

--- JHMDB/Main.py
+++ JHMDB/Main.py
@@ -256,7 +256,7 @@ if __name__ == '__main__':
     total_loss = 0.0
     total_correct = 0
     epochs = 80
-    batch_size = 40
+    batch_size = 80
     l = []
     tot_loss = 0
     accuracy = []
