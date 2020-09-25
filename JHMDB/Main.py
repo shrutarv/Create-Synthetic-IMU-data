b2e7d6d81da0acf0557cbcc8812511889702a820
shrutarv <shrutarv.awasthi@gmail.com>
2020-09-25 14:12:26 +0530

    path changes

--- JHMDB/Main.py
+++ JHMDB/Main.py
@@ -255,7 +255,7 @@ if __name__ == '__main__':
     correct = 0
     total_loss = 0.0
     total_correct = 0
-    epochs = 200
+    epochs = 100
     batch_size = 20
     l = []
     tot_loss = 0
@@ -280,7 +280,7 @@ if __name__ == '__main__':
     model_path = '/data/sawasthi/data/JHMDB/model/model.pth'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
-    path = '/data/sawasthi/data/JHMDB/trainData_a/'
+    path = '/data/sawasthi/data/JHMDB/trainData_a_acc/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
@@ -293,7 +293,7 @@ if __name__ == '__main__':
   
    
     # Validation data    
-    path = '/data/sawasthi/data/JHMDB/validationData_a/'
+    path = '/data/sawasthi/data/JHMDB/validationData_a_acc/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
@@ -305,7 +305,7 @@ if __name__ == '__main__':
                                    drop_last=True)
     
     # Test data    
-    path = '/data/sawasthi/data/JHMDB/testData_a/'
+    path = '/data/sawasthi/data/JHMDB/testData_a_acc/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
     test_dataset = CustomDataSet(path)
@@ -397,7 +397,7 @@ if __name__ == '__main__':
     plt.plot(ep,accuracy,label='training accuracy')
     plt.plot(ep,validation_acc, label='validation loss')
     plt.legend()
-    plt.savefig('/data/sawasthi/data/JHMDB/results/result_a.png') 
+    plt.savefig('/data/sawasthi/data/JHMDB/results/result_a_acc.png') 
     #plt.savefig('S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/result.png') 
     #plt.savefig('S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/result.png')
     
@@ -447,7 +447,7 @@ if __name__ == '__main__':
     print('Finished Validation')
     #with open('S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/result.csv', 'w', newline='') as myfile:
     #with open('S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/result.csv', 'w', newline='') as myfile:
-    with open('/data/sawasthi/data/JHMDB/results/result_a.csv', 'w') as myfile:
+    with open('/data/sawasthi/data/JHMDB/results/result_a_acc.csv', 'w') as myfile:
          wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
          wr.writerow(accuracy)
          wr.writerow(l)
--- JHMDB/op_sliding_window.py
+++ JHMDB/op_sliding_window.py
@@ -168,7 +168,7 @@ with open("S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/norm_values.csv
     fc.writerows(value)
 plt.plot(data[:,0],data[:,1])
 '''
-'''
+
 data = normalize(data,value, "train")
 print("train data normalized")
 # time sampled
@@ -176,7 +176,7 @@ x_sampled = np.linspace(np.min(data[:,0]), np.max(data[:,0]), len(data)*up)
 y_sampled = np.zeros((len(x_sampled),1))
 #sampled_data = []
 #y_sampled2 = np.zeros((len(x_sampled),1))
-'''
+
 '''
 for i in range(1,(data.shape[1]-1)):
     #for index in range(12,len(data[0])*up-12):
@@ -192,7 +192,6 @@ for i in range(1,(data.shape[1]-1)):
      
      #y_sampled.append(f(x_sampled))
      # plt.plot(data[1:10,0],data[1:10,i],'o',x_new[1:10],y_new,'x')
-'''
 '''
 for i in range(1,(data.shape[1]-1)):
      print(i)
@@ -232,7 +231,7 @@ X = data_new
 k = 0
 example_creating_windows_file(k, X, lab, data_dir)
 print("train data pickled")
-'''
+
 #data_dir = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl'
 data_dir =  '/data/sawasthi/data/JHMDB/testData_a_acc/'
 df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/test_data.csv')
@@ -246,7 +245,7 @@ for i in range(1,(data.shape[1]-1)):
      print(i)
      acc = np.array([0], float)
      for index in range(50,len(data)-49):
-         print(index)
+        
          data_new = data[index-50:index+50,:]   
          x_samp = np.linspace(np.min(data_new[:,0]), np.max(data_new[:,0]), len(data_new)*up)
          #sampled_data = f(x_sampled[index-50:index+50,:])
@@ -287,7 +286,7 @@ for i in range(1,(data.shape[1]-1)):
      print(i)
      acc = np.array([0], float)
      for index in range(50,len(data)-49):
-         print(index)
+         
          data_new = data[index-50:index+50,:]   
          x_samp = np.linspace(np.min(data_new[:,0]), np.max(data_new[:,0]), len(data_new)*up)
          #sampled_data = f(x_sampled[index-50:index+50,:])
