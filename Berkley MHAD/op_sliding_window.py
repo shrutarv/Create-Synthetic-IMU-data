163acb326c088b7d2fe2da26801d0f4a142f0a9f
shrutarv <shrutarv.awasthi@gmail.com>
2020-10-01 16:34:43 +0530

    Update op_sliding_window.py

--- Berkley MHAD/op_sliding_window.py
+++ Berkley MHAD/op_sliding_window.py
@@ -91,9 +91,9 @@ def example_creating_windows_file(k, data_x, labels, data_dir):
    # for f in range(X.shape[0]):
     for f in range(value):
        # try:
-        #sys.stdout.write('\r' + 'Creating sequence file '
-         #                       'number {} with id {}'.format(f, counter_seq))
-        #sys.stdout.flush()
+        sys.stdout.write('\r' + 'Creating sequence file '
+                                'number {} with id {}'.format(f, counter_seq))
+        sys.stdout.flush()
 
         # print "Creating sequence file number {} with id {}".format(f, counter_seq)
         seq = np.reshape(X[f], newshape = (1, X.shape[1], X.shape[2]))
@@ -226,9 +226,9 @@ if __name__ == '__main__':
         print("test data normalized")
         # time sampled
         #x_sampled = np.linspace(np.min(data[:,0]), np.max(data[:,0]), len(data)*up)
-        
+       
         y_sampled = np.zeros((int(np.ceil(data.shape[0]/float(down))),1))
-        print(y_sampled.shape)        
+             
         for i in range(data_norm.shape[1]):
             #for index in range(12,len(data[0])*up-12):
                     
@@ -273,7 +273,7 @@ if __name__ == '__main__':
         print("validation data normalized")
         # time sampled
         #x_sampled = np.linspace(np.min(data[:,0]), np.max(data[:,0]), len(data)*up)
-        y_sampled = np.zeros((int(np.ceil(data.shape[0]/down)),1))
+        y_sampled = np.zeros((int(np.ceil(data.shape[0]/float(down))),1))
                 
         for i in range(data_norm.shape[1]):
             #for index in range(12,len(data[0])*up-12):
