63aed66478d47d4ea0f4846015d6bd5af96de3bd
shrutarv <shrutarv.awasthi@gmail.com>
2020-10-08 16:35:21 +0530

    Update op_sliding_window_acc.py

--- MoCAP/op_sliding_window_acc.py
+++ MoCAP/op_sliding_window_acc.py
@@ -301,7 +301,7 @@ if __name__ == '__main__':
     # validation set : S05,S11,S12
     # test set : S06,13,14
      #for i in sliding_window(data_y,(ws,data_y.shape[1]),(ss,1)):
-    FolderList = ['P05','P11','P12']
+    FolderList = ['P06','P13','P14']
     #    print (np.shape(i[:,0]))
     #folder_name = "P09"
     for folder_name in FolderList:
@@ -332,5 +332,5 @@ if __name__ == '__main__':
            
             trainData = np.concatenate((trainData,np.reshape(labels,(len(labels),1))))
          
-    np.savetxt("/data/sawasthi/data/MoCAP_data/train_csv/validationLabels.csv", trainData, delimiter=',')
+    np.savetxt("/data/sawasthi/data/MoCAP_data/train_csv/testLabels.csv", trainData, delimiter=',')
     
