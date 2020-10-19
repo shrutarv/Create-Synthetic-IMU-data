8bdab019863b601d5d736c4a33ae82a2bcdd1508
shrutarv <shrutarv.awasthi@gmail.com>
2020-10-19 18:31:04 +0530

    Update op_sliding_window.py

--- JHMDB/op_sliding_window.py
+++ JHMDB/op_sliding_window.py
@@ -207,11 +207,11 @@ if __name__ == '__main__':
     up = 4
     #ws = (100,31)
     ws = (100,30) 
-    ss = (10,30)     
+    ss = (2,30)     
     #ss = (25,31)
     sliding_window_length = 100   
     #sliding_window_length = 100    
-    sliding_window_step = 10
+    sliding_window_step = 2
     
     df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/train_data.csv')
     #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/train_data.csv')
@@ -241,7 +241,7 @@ if __name__ == '__main__':
     
     #data_dir = "/media/shrutarv/Drive1/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows2/"
     #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/train_data25_39.csv')
-    data_dir =  '/data/sawasthi/data/JHMDB/trainData_n_10ss/'
+    data_dir =  '/data/sawasthi/data/JHMDB/trainData_2ss/'
     #data_dir = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     label = np.repeat(data[:,31],up).astype(int)
     lab = np.zeros((len(label),20), dtype=int)
@@ -253,7 +253,7 @@ if __name__ == '__main__':
     print("train data pickled")
     
     #data_dir = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl'
-    data_dir =  '/data/sawasthi/data/JHMDB/testData_n_10ss/'
+    data_dir =  '/data/sawasthi/data/JHMDB/testData_2ss/'
     df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/test_data.csv')
     data = df.values
     data = normalize(data,value, "test")
@@ -285,7 +285,7 @@ if __name__ == '__main__':
     example_creating_windows_file(k, X, lab, data_dir)
     print("test data pickled")
     
-    data_dir =  '/data/sawasthi/data/JHMDB/validationData_n_10ss/'
+    data_dir =  '/data/sawasthi/data/JHMDB/validationData_2ss/'
     #data_dir =  '/data/sawasthi/data/JHMDB/validationData/'
     df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/validation_data.csv')
     data = df.values
