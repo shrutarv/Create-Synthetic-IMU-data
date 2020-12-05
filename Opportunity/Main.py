fce7c39df45504e9719371f88cd643338af46872
shrutarv <shrutarv.awasthi@gmail.com>
2020-12-05 12:06:23 +0530

    Update Main.py

--- Opportunity/Main.py
+++ Opportunity/Main.py
@@ -412,7 +412,7 @@ if __name__ == '__main__':
             
         for b, harwindow_batched in enumerate(dataLoader_test):
             test_batch_v = harwindow_batched["data"]
-            test_batch_l = harwindow_batched["label"][:, 0]
+            test_batch_l = harwindow_batched["label"]
            # test_batch_v = normalize(test_batch_v, value,"test")
             test_batch_v = test_batch_v.float()
             test_batch_v = test_batch_v.to(device)
