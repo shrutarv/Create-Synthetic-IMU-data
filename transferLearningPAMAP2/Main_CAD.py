64e7bb2cfd2a9bba8bcf0874638967d7de539090
shrutarv <shrutarv.awasthi@gmail.com>
2020-12-15 10:46:43 +0530

    Update Main_CAD.py

--- transferLearningPAMAP2/Main_CAD.py
+++ transferLearningPAMAP2/Main_CAD.py
@@ -314,7 +314,7 @@ if __name__ == '__main__':
         "output":"softmax",
         "num_classes":16,
         "reshape_input":False,
-        "folder_exp_base_fine_tuning": '/home/sawasthi/CAD60/model/model_acc_test.pth'
+        "folder_exp_base_fine_tuning": '/home/sawasthi/CAD60/model/model_adap.pth'
         #"folder_exp_base_fine_tuning": 'S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/model_full.pth'
         }
 
@@ -324,7 +324,7 @@ if __name__ == '__main__':
     correct = 0
     total_loss = 0.0
     total_correct = 0
-    epochs = 40
+    epochs = 1
     batch_size = 40
     l = []
     tot_loss = 0
