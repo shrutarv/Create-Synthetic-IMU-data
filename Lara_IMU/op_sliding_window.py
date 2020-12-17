c1be33a1cb4825f79e712719603ab38af7253459
shrutarv <shrutarv.awasthi@gmail.com>
2020-12-17 14:20:15 +0530

    Update op_sliding_window.py

--- Lara_IMU/op_sliding_window.py
+++ Lara_IMU/op_sliding_window.py
@@ -154,7 +154,8 @@ if __name__ == '__main__':
     # training set : S01, S02,S03,S04,S07,S08,S09,S10
     # validation set : S05,S11,S12
     # test set : S06,13,14
-    df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/Github/Thesis- Create Synthetic IMU data/Lara_IMU/norm_IMU.csv')
+    #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/Github/Thesis- Create Synthetic IMU data/Lara_IMU/norm_IMU.csv')
+    df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/Lara_IMU/norm_IMU.csv')
     value = df.values.tolist()
    
     data_dir =  "/data/sawasthi/data/Lara_IMU/trainData/"
