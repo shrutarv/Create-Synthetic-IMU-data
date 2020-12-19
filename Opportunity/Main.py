6ddc84c2d920610093571e10d67ad8acf6ee43b3
shrutarv <shrutarv.awasthi@gmail.com>
2020-12-19 16:16:14 +0530

    Update Main.py

--- Opportunity/Main.py
+++ Opportunity/Main.py
@@ -256,7 +256,7 @@ if __name__ == '__main__':
     total_loss = 0.0
     total_correct = 0
     epochs = 150
-    batch_size = 200
+    batch_size = 100
     l = []
     tot_loss = 0
     accuracy = []
