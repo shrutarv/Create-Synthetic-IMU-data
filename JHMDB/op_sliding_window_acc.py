7c8c98b3f7869f48a4079293077e51ce3e9a3374
shrutarv <shrutarv.awasthi@gmail.com>
2020-12-21 16:38:02 +0530

    Update op_sliding_window_acc.py

--- JHMDB/op_sliding_window_acc.py
+++ JHMDB/op_sliding_window_acc.py
@@ -204,14 +204,14 @@ if __name__ == '__main__':
     # The training, test and validation data have been separately interpolated and 
     # up sampled
     # up sampling rate
-    up = 1
+    up = 4
     #ws = (100,31)
-    ws = (25,30) 
-    ss = (5,30)     
+    ws = (100,30) 
+    ss = (12,30)     
     #ss = (25,31)
-    sliding_window_length = 25   
+    sliding_window_length = 100   
     #sliding_window_length = 100    
-    sliding_window_step = 5
+    sliding_window_step = 12
     
     df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/train_data.csv')
     #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/train_data.csv')
@@ -283,7 +283,7 @@ if __name__ == '__main__':
     
     #data_dir = "/media/shrutarv/Drive1/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows2/"
     #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/train_data25_39.csv')
-    data_dir =  '/data/sawasthi/data/JHMDB/trainData_up1/'
+    data_dir =  '/data/sawasthi/data/JHMDB/trainData_up4/'
     #data_dir = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     label = np.repeat(data[:,31],up).astype(int)
     lab = np.zeros((len(label),20), dtype=int)
@@ -295,7 +295,7 @@ if __name__ == '__main__':
     print("train data pickled")
     
     #data_dir = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl'
-    data_dir =  '/data/sawasthi/data/JHMDB/testData_up1/'
+    data_dir =  '/data/sawasthi/data/JHMDB/testData_up4/'
     df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/test_data.csv')
     data = df.values
     data = normalize(data,value, "test")
@@ -327,7 +327,7 @@ if __name__ == '__main__':
     example_creating_windows_file(k, X, lab, data_dir)
     print("test data pickled")
     
-    data_dir =  '/data/sawasthi/data/JHMDB/validationData_up1/'
+    data_dir =  '/data/sawasthi/data/JHMDB/validationData_up4/'
     #data_dir =  '/data/sawasthi/data/JHMDB/validationData/'
     df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/validation_data.csv')
     data = df.values
