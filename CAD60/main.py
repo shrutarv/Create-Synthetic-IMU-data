d2b44b344ad245aa0c0497f93eecfd9bb16cb993
shrutarv <shrutarv.awasthi@gmail.com>
2020-12-22 03:15:57 +0530

    Update main.py

--- CAD60/main.py
+++ CAD60/main.py
@@ -256,7 +256,7 @@ if __name__ == '__main__':
     total_loss = 0.0
     total_correct = 0
     epochs = 160
-    batch_size = 20
+    batch_size = 100
     lr_factor = 0.98
     l = []
     tot_loss = 0
