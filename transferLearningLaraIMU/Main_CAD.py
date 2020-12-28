4e2762005b58eb25aacebb7fb8d737e89ae56fab
shrutarv <shrutarv.awasthi@gmail.com>
2020-12-28 19:50:31 +0530

    Update Main_CAD.py

--- transferLearningLaraIMU/Main_CAD.py
+++ transferLearningLaraIMU/Main_CAD.py
@@ -237,7 +237,7 @@ def set_required_grad(network):
 
         if config["network"] == 'cnn':
             list_layers = ['conv1_1.weight', 'conv1_1.bias','conv1_2.weight', 'conv1_2.bias',
-                          'conv2_1.weight', 'conv2_1.bias']
+                          'conv2_1.weight', 'conv2_1.bias','conv2_2.weight', 'conv2_2.bias']
         elif config["network"] == 'cnn_imu':
             list_layers = ['conv_LA_1_1.weight', 'conv_LA_1_1.bias', 'conv_LA_1_2.weight', 'conv_LA_1_2.bias',
                            'conv_LA_2_1.weight', 'conv_LA_2_1.bias', 'conv_LA_2_2.weight', 'conv_LA_2_2.bias',
@@ -271,7 +271,7 @@ def load_weights(network):
 
         if config["network"] == 'cnn':
             list_layers = ['conv1_1.weight', 'conv1_1.bias','conv1_2.weight', 'conv1_2.bias',
-                           'conv2_1.weight', 'conv2_1.bias']
+                           'conv2_1.weight', 'conv2_1.bias','conv2_2.weight', 'conv2_2.bias']
         elif config["network"] == 'cnn_imu':
             list_layers = ['conv_LA_1_1.weight', 'conv_LA_1_1.bias', 'conv_LA_1_2.weight', 'conv_LA_1_2.bias',
                            'conv_LA_2_1.weight', 'conv_LA_2_1.bias', 'conv_LA_2_2.weight', 'conv_LA_2_2.bias',
@@ -324,12 +324,12 @@ if __name__ == '__main__':
     correct = 0
     total_loss = 0.0
     total_correct = 0
-    epochs = 120
-    batch_size = 100
+    epochs = 140
+    batch_size = 400
     l = []
     tot_loss = 0
     accuracy = []
-    learning_rate = 0.00001
+    learning_rate = 0.0001
     print("sliding_window_length", config["sliding_window_length"],"epoch: ",epochs,"batch_size: ",batch_size,"accumulation steps: ",accumulation_steps,"ws: ",ws, "learning_rate: ",learning_rate)
         
     #df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/MoCAP/norm_values.csv')
