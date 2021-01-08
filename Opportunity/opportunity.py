afe4c3a7deeb2dd0d191987be09065131aa7ff9b
shrutarv <shrutarv.awasthi@gmail.com>
2021-01-09 00:13:19 +0530

    Update opportunity.py

--- Opportunity/opportunity.py
+++ Opportunity/opportunity.py
@@ -281,8 +281,8 @@ class Opportunity(data.Dataset):
             if self.config['dataset'] == 'locomotion':
                 print('adjust_idx_labels locomotion')# Labels for locomotion are adjusted
                 print('all data_y values',np.unique(data_y))
-                data_y[data_y == 4] = 3
-                data_y[data_y == 5] = 4
+                data_y[np.where(data_y == 4)[0]] = 3
+                data_y[np.where(data_y == 5)[0]] = 4
                 print('all data_y values',np.unique(data_y))
                 print('data_y max l{}'.format(np.max(data_y)))
             elif self.config['dataset'] == 'gesture':  # Labels for gestures are adjusted
