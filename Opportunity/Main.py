cd417b60fb64a4b4b2e32f6da435de37abea944f
shrutarv <shrutarv.awasthi@gmail.com>
2021-01-08 23:27:58 +0530

    Update Main.py

--- Opportunity/Main.py
+++ Opportunity/Main.py
@@ -187,9 +187,11 @@ def validation(dataLoader_validation):
         correct = (trueValue == prediction).sum().item()
         
         #counter = out.view(-1, n_classes).size(0)
-    print('\nvalidation set: test batch l{}\n'.format(test_batch_l))  
-    print('\nvalidation set: test batch l{}\n'.format(harwindow_batched["label"]))  
-    print('output: l{}'.format(out))
+    print('\nvalidation set: test batch l{}\n'.format(test_batch_l[0]))  
+    print('\nvalidation set: test batch l{}\n'.format(harwindow_batched["label"][0]))  
+    print('output: l{}'.format(out)[0])
+    print('output: l{}'.format(predicted_classes)[0])
+    
     print('\nValidation set:  Percent Validation Accuracy: {:.4f}\n'.format(100. * correct / float(total)))
     return (100. * correct / float(total), total_loss/float((b+1)))
         
