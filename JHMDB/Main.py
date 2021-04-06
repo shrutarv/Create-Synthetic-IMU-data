16fb8ab47540bf9319b61f41026633946da51894
shrutarv <shrutarv.awasthi@gmail.com>
2021-04-06 19:56:01 +0200

    Update Main.py

--- JHMDB/Main.py
+++ JHMDB/Main.py
@@ -284,7 +284,7 @@ if __name__ == '__main__':
     #scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, patience=5)
     
     #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
-    model_path = '/data/sawasthi/data/JHMDB/model/model_acc_test.pth'
+    model_path = '/data/sawasthi/JHMDB/model/model_acc_test.pth'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
     path = '/data/sawasthi/JHMDB/trainData_50a/'
