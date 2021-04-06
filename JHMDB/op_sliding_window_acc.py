3b4d3447ddb45e5b57808d43f4cb383a01fe6ce4
shrutarv <shrutarv.awasthi@gmail.com>
2021-04-06 22:52:17 +0200

    Update op_sliding_window_acc.py

--- JHMDB/op_sliding_window_acc.py
+++ JHMDB/op_sliding_window_acc.py
@@ -296,7 +296,7 @@ if __name__ == '__main__':
     
     #data_dir = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl'
     data_dir =  '/data/sawasthi/JHMDB/testData_75a/'
-    df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/test_data.csv')
+    df = pd.read_csv('/home/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/test_data.csv')
     data = df.values
     data = normalize(data,value, "test")
     print("test data normalized")
@@ -329,7 +329,7 @@ if __name__ == '__main__':
     
     data_dir =  '/data/sawasthi/JHMDB/validationData_75a/'
     #data_dir =  '/data/sawasthi/data/JHMDB/validationData/'
-    df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/validation_data.csv')
+    df = pd.read_csv('/home/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/validation_data.csv')
     data = df.values
     data = normalize(data,value, "validation")
     print("validation data normalized")
