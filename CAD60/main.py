9ef69c0134230ef2432aff6ed42a4c223daa5f07
shrutarv <shrutarv.awasthi@gmail.com>
2021-04-07 23:12:01 +0200

    Update main.py

--- CAD60/main.py
+++ CAD60/main.py
@@ -307,7 +307,7 @@ if __name__ == '__main__':
                                    drop_last=True)
     
     # Test data    
-    path = '/data/sawasthi/data/CAD60/testData_ss_25/'
+    path = '/data/sawasthi/CAD60/testData_ss_25/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
     test_dataset = CustomDataSet(path)
