0ff631f45e5b36fef0ead538b54c861ca6689375
shrutarv <shrutarv.awasthi@gmail.com>
2021-04-12 22:30:24 +0200

    Update op_sliding_window.py

--- CAD60/op_sliding_window.py
+++ CAD60/op_sliding_window.py
@@ -174,11 +174,11 @@ if __name__ == '__main__':
     up = 3
     #ws = (100,31)
     ws = (100,45) 
-    ss = (5,45)     
+    ss = (1,45)     
     #ss = (25,31)
     sliding_window_length = 100   
     #sliding_window_length = 100    
-    sliding_window_step = 5
+    sliding_window_step = 1
     df = pd.read_csv('/data/sawasthi/CAD60/train_data.csv')
    # df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/CAD 60/train_data.csv')
     data = df.values
@@ -258,7 +258,7 @@ if __name__ == '__main__':
     
     #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/train_data25_39.csv')
     #data_dir =  'S:/MS A&R/4th Sem/Thesis/CAD 60/pkl/'
-    data_dir =  '/data/sawasthi/CAD60/trainData_ss_5/'
+    data_dir =  '/data/sawasthi/CAD60/trainData_ss_1/'
     label = np.repeat(data[:,46],up).astype(int)
     lab = np.zeros((len(label),20), dtype=int)
     lab[:,0] = label
@@ -268,7 +268,7 @@ if __name__ == '__main__':
     example_creating_windows_file(k, X, lab, data_dir)
     print("train data pickled")
     
-    data_dir =  '/data/sawasthi/CAD60/testData_ss_5'
+    data_dir =  '/data/sawasthi/CAD60/testData_ss_1'
     df = pd.read_csv('/data/sawasthi/CAD60/test_data.csv')
     data = df.values
     data = normalize(data,value, "test")
@@ -302,7 +302,7 @@ if __name__ == '__main__':
     print("test data pickled")
     
     df = pd.read_csv('/data/sawasthi/CAD60/validation_data.csv')
-    data_dir =  '/data/sawasthi/CAD60/validationData_ss_5/'
+    data_dir =  '/data/sawasthi/CAD60/validationData_ss_1/'
     data = df.values
     data = normalize(data,value, "validation")
     print("validation data normalized")
