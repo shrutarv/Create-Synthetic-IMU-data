9b9173900e9852542900d5856e8882b37f7b0c46
shrutarv <shrutarv.awasthi@gmail.com>
2021-04-23 12:34:45 +0200

    Update Main.py

--- JHMDB/Main.py
+++ JHMDB/Main.py
@@ -568,9 +568,9 @@ if __name__ == '__main__':
 
     ws=100
     accumulation_steps = 5
-    epochs = 100
+    epochs = 200
     batch_size = 50
-    learning_rate = 0.00001
+    learning_rate = 0.0001
     print("epoch: ",epochs,"batch_size: ", batch_size,"accumulation steps: ",accumulation_steps,"ws: ",ws, "learning_rate: ",learning_rate)
         
     #df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/MoCAP/norm_values.csv')
