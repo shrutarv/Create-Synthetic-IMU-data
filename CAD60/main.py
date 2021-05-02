d833976ae75ed73fdb64de34a1ba6b47d0cb72ed
shrutarv <shrutarv.awasthi@gmail.com>
2021-05-03 00:01:55 +0200

    Update main.py

--- CAD60/main.py
+++ CAD60/main.py
@@ -507,7 +507,7 @@ if __name__ == '__main__':
         "output":"softmax",
         "num_classes":12,
         "reshape_input":False,
-        "step_size":50
+        "step_size":12
         }
 
 
@@ -540,11 +540,11 @@ if __name__ == '__main__':
     #optimizer = optim.Adam(model.parameters(), lr=0.001)
     optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, alpha=0.9,weight_decay=0.0005, momentum=0.9)
     #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
-    model_path = '/data/sawasthi/CAD60/model/model_tf.pth'
+    model_path = '/data/sawasthi/CAD60/model/model_tf_12.pth'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
    
-    path = '/data/sawasthi/CAD60/trainData_50_tf/'
+    path = '/data/sawasthi/CAD60/trainData_tf/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
@@ -557,7 +557,7 @@ if __name__ == '__main__':
   
     
     # Validation data    
-    path = '/data/sawasthi/CAD60/validationData_50_tf/'
+    path = '/data/sawasthi/CAD60/validationData_tf/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
