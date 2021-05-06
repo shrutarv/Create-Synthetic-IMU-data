00af34c52731fdb9508fe09f14d12adb828e3658
shrutarv <shrutarv.awasthi@gmail.com>
2021-05-06 20:56:19 +0200

    Update Main.py

--- transferLearning_Opportunity/Main.py
+++ transferLearning_Opportunity/Main.py
@@ -312,7 +312,7 @@ if __name__ == '__main__':
         "output":"softmax",
         "num_classes":18,
         "reshape_input":False,
-        "folder_exp_base_fine_tuning": '/data/sawasthi/data/JHMDB/model/model_acc_up1.pth'
+        "folder_exp_base_fine_tuning": '/data/sawasthi/data/JHMDB/model/model_acc_up4.pth'
         #"folder_exp_base_fine_tuning": 'S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/model_full.pth'
         }
 
