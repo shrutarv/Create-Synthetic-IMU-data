c16a63134e232c2d887ba4c6958dba4d333a8784
shrutarv <shrutarv.awasthi@gmail.com>
2021-05-11 16:27:23 +0200

    edit

--- Opportunity/src/test_shrutarv.py
+++ Opportunity/src/test_shrutarv.py
@@ -6,7 +6,7 @@ Created on Tue May 11 14:44:34 2021
 """
 import torch
 
-pretrained_dict = torch.load('/data/sawasthi/CAD60/model/model_tf_12.pth').state_dict()
+pretrained_dict = torch.load('/data/sawasthi/CAD60/model/model_test.pth').state_dict()
 
 torch.save({'state_dict': pretrained_dict},
                        '/data/sawasthi/CAD60/model/' + 'network.pt')     
