345ebbc850e3775e5f91868fd9a37d4ed714ea57
shrutarv <shrutarv.awasthi@gmail.com>
2021-05-12 21:42:45 +0200

    Update Main_Penn.py

--- transferLearningLaraIMU/Main_Penn.py
+++ transferLearningLaraIMU/Main_Penn.py
@@ -482,7 +482,7 @@ if __name__ == '__main__':
         #"folder_exp_base_fine_tuning": 'S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/model_full.pth'
         }
     flag = True
-    iterations = 5
+    iterations = 1
     weighted_F1_array = []
     test_acc_array = []
     for iter in range(iterations):
