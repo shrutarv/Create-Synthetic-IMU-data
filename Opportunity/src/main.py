3753e0677b43f339bd8832e393b0862a8493e8c8
shrutarv <shrutarv.awasthi@gmail.com>
2021-05-16 09:07:31 +0200

    4 conv layers

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -227,7 +227,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                      'freeze_options': freeze_options[freeze],
                      'proportions': proportions[proportions_id],
                      'fully_convolutional': fully_convolutional,
-                     'model_path': '/data/sawasthi/Opportunity/model/network_CAD_c1_c2_c3.pt'}
+                     'model_path': '/data/sawasthi/Opportunity/model/network_CAD_c1_c2_c3_c4.pt'}
     
     return configuration
 
@@ -280,7 +280,7 @@ def pamap2_main():
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=0, name_counter=0,
                                                freeze=fopt, proportions_id=pp, gpudevice="1")
                         setup_experiment_logger(logging_level=logging.DEBUG,
-                                                filename=config['folder_exp'] + "logger_c1_c2.txt")
+                                                filename=config['folder_exp'] + "logger_c1_c2_c3_c4.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
@@ -307,7 +307,7 @@ def locomotion_main():
                         config = configuration(dataset_idx=dset, network_idx=arch, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=0, name_counter=0,
                                            freeze=1, proportions_id = pp, gpudevice = "0")
-                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_CAD_c1_c2_c3.txt")
+                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_CAD_c1_c2_c3_c4.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
