45c049d5148e156f2e7688cbc0620184c692ea4f
shrutarv <shrutarv.awasthi@gmail.com>
2021-05-16 09:38:20 +0200

    Update test_shrutarv.py

--- Opportunity/src/test_shrutarv.py
+++ Opportunity/src/test_shrutarv.py
@@ -6,7 +6,7 @@ Created on Tue May 11 14:44:34 2021
 """
 import torch
 
-pretrained_dict = torch.load('/data/sawasthi/Penn/model/model_tf.pth').state_dict()
+pretrained_dict = torch.load('/data/sawasthi/Penn/model/model_tf.pth')
 
 torch.save({'state_dict': pretrained_dict},
                        '/data/sawasthi/Penn/model/network.pt')     
