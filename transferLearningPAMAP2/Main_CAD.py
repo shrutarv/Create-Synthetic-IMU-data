a292cc7e3d3cff34f18d8369b69eb264386b31c2
shrutarv <shrutarv.awasthi@gmail.com>
2021-05-20 19:31:33 +0200

    Update Main_CAD.py

--- transferLearningPAMAP2/Main_CAD.py
+++ transferLearningPAMAP2/Main_CAD.py
@@ -514,12 +514,12 @@ if __name__ == '__main__':
         model.fc5 = PAMAP_net.fc5
         model.softmax = PAMAP_net.softmax
         '''
-        model = set_required_grad(model)
+       # model = set_required_grad(model)
         #optimizer = optim.Adam(model.parameters(), lr=0.001)
         optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, alpha=0.9,weight_decay=0.0005, momentum=0.9)
         optimizer.zero_grad()
         #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
-        path = '/data/sawasthi/PAMAP2/trainData_30/'
+        path = '/data/sawasthi/PAMAP2/trainData/'
         #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
         #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
