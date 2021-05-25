5f9fc1c057579a899c784348b2a4074d6f709ca9
shrutarv <shrutarv.awasthi@gmail.com>
2021-05-25 18:09:39 +0200

    Update Main.py

--- transferLearningLaraIMU/Main.py
+++ transferLearningLaraIMU/Main.py
@@ -236,7 +236,7 @@ def set_required_grad(network):
         logging.info('        Network_User:        Setting Required_grad to Weights')
 
         if config["network"] == 'cnn':
-            list_layers = ['conv1_1.weight', 'conv1_1.bias']
+            list_layers = ['conv1_1.weight', 'conv1_1.bias','conv1_2.weight', 'conv1_2.bias']
         elif config["network"] == 'cnn_imu':
             list_layers = ['conv_LA_1_1.weight', 'conv_LA_1_1.bias', 'conv_LA_1_2.weight', 'conv_LA_1_2.bias',
                            'conv_LA_2_1.weight', 'conv_LA_2_1.bias', 'conv_LA_2_2.weight', 'conv_LA_2_2.bias',
@@ -269,7 +269,7 @@ def load_weights(network):
         #    print(k)
 
         if config["network"] == 'cnn':
-            list_layers = ['conv1_1.weight', 'conv1_1.bias']
+            list_layers = ['conv1_1.weight', 'conv1_1.bias','conv1_2.weight', 'conv1_2.bias']
         elif config["network"] == 'cnn_imu':
             list_layers = ['conv_LA_1_1.weight', 'conv_LA_1_1.bias', 'conv_LA_1_2.weight', 'conv_LA_1_2.bias',
                            'conv_LA_2_1.weight', 'conv_LA_2_1.bias', 'conv_LA_2_2.weight', 'conv_LA_2_2.bias',
@@ -453,7 +453,7 @@ if __name__ == '__main__':
         }
 
     flag = True
-    iterations = 5
+    iterations = 2
     weighted_F1_array = []
     test_acc_array = []
     for iter in range(iterations):
@@ -545,7 +545,7 @@ if __name__ == '__main__':
             data_x.to(device)
             value = max_min_values(data_x,value)
         '''
-        model_path_tl = '/data/sawasthi/LaraIMU/model/model_tl_JHMDB_LIMU_p.pth'
+        model_path_tl = '/data/sawasthi/LaraIMU/model/model_tl_JHMDB_LIMU_p_c1_c2.pth'
         print('Start Training')
                  
         training(dataLoader_train, dataLoader_validation,device,flag)
