4891e0b8194cf21f4ee45961130c578d8110dbcf
shrutarv <shrutarv.awasthi@gmail.com>
2021-05-26 18:07:57 +0200

    Update Main_CAD.py

--- transferLearningLara_motionminers/Main_CAD.py
+++ transferLearningLara_motionminers/Main_CAD.py
@@ -444,7 +444,7 @@ if __name__ == '__main__':
     random.seed(seed)
 
     if torch.cuda.is_available():  
-          dev = "cuda:2" 
+          dev = "cuda:1" 
     else:  
           dev = "cpu"  
           
