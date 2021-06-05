a549a48e58c8a7a2baa3dbcee7340c6d677cbfc6
shrutarv <shrutarv.awasthi@gmail.com>
2021-06-05 19:49:02 +0200

    ges

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -146,7 +146,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
         '''
     elif usage_modus[usage_modus_idx] == 'fine_tuning':
         folder_exp = '/data/sawasthi/Opportunity/model/'
-        folder_exp_base_fine_tuning = '/data/sawasthi/JHMDB/model/model_opp_tf_pose.pt'
+        folder_exp_base_fine_tuning = '/data/sawasthi/Penn/model/model_opp_tf_pose.pt'
         '''
         folder_exp = '/data2/fmoya/HAR/pytorch/' + dataset[dataset_idx] + '/' + \
                      network[network_idx] + '/' + output[output_idx] + '/' + fully_convolutional + \
@@ -227,7 +227,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                      'freeze_options': freeze_options[freeze],
                      'proportions': proportions[proportions_id],
                      'fully_convolutional': fully_convolutional,
-                     'model_path': '/data/sawasthi/Opportunity/model/network_JHMDB_ges_c1_75_pose.pt'}
+                     'model_path': '/data/sawasthi/Opportunity/model/network_Penn_ges_c1_pose.pt'}
     
     return configuration
 
@@ -332,8 +332,8 @@ def gestures_main():
                     for pp in proportions_opts:
                         config = configuration(dataset_idx=1, network_idx=arch, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=0, name_counter=0,
-                                               freeze=1, proportions_id = 2, gpudevice = "0")
-                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_JHMDB_ges_c1_75_pose.txt")
+                                               freeze=1, proportions_id = 3, gpudevice = "0")
+                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_Penn_ges_c1_pose.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
