d5ad6b763900ac47dcacfe0b867a1a15734ecc12
shrutarv <shrutarv.awasthi@gmail.com>
2021-06-08 07:51:05 +0200

    Update op_sliding_window.py

--- CAD60/op_sliding_window.py
+++ CAD60/op_sliding_window.py
@@ -179,7 +179,7 @@ if __name__ == '__main__':
     sliding_window_length = 100   
     #sliding_window_length = 100    
     sliding_window_step = 12
-    df = pd.read_csv('/data/sawasthi/CAD60/train_data.csv')
+    df = pd.read_csv('/data/sawasthi/data/CAD60/train_data.csv')
    # df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/CAD 60/train_data.csv')
     data = df.values
     data_new = data[:,1:46]
@@ -258,7 +258,7 @@ if __name__ == '__main__':
     
     #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/train_data25_39.csv')
     #data_dir =  'S:/MS A&R/4th Sem/Thesis/CAD 60/pkl/'
-    data_dir =  '/data/sawasthi/CAD60/trainData_acc_up1/'
+    data_dir =  '/data/sawasthi/data/CAD60/trainData_acc_up1/'
     label = np.repeat(data[:,46],up).astype(int)
     lab = np.zeros((len(label),20), dtype=int)
     lab[:,0] = label
@@ -268,8 +268,8 @@ if __name__ == '__main__':
     example_creating_windows_file(k, X, lab, data_dir)
     print("train data pickled")
     
-    data_dir =  '/data/sawasthi/CAD60/testData_opp_acc_up1/'
-    df = pd.read_csv('/data/sawasthi/CAD60/test_data.csv')
+    data_dir =  '/data/sawasthi/data/CAD60/testData_opp_acc_up1/'
+    df = pd.read_csv('/data/sawasthi/data/CAD60/test_data.csv')
     data = df.values
     data = normalize(data,value, "test")
     print("test data normalized")
@@ -301,8 +301,8 @@ if __name__ == '__main__':
     example_creating_windows_file(k, X, lab, data_dir)
     print("test data pickled")
     
-    df = pd.read_csv('/data/sawasthi/CAD60/validation_data.csv')
-    data_dir =  '/data/sawasthi/CAD60/validationData_opp_acc_up1/'
+    df = pd.read_csv('/data/sawasthi/data/CAD60/validation_data.csv')
+    data_dir =  '/data/sawasthi/data/CAD60/validationData_opp_acc_up1/'
     data = df.values
     data = normalize(data,value, "validation")
     print("validation data normalized")
