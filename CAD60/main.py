a12fd31adf3d1752b189c23f67f57cc155f97f21
shrutarv <shrutarv.awasthi@gmail.com>
2021-06-08 06:42:07 +0200

    Update main.py

--- CAD60/main.py
+++ CAD60/main.py
@@ -574,7 +574,7 @@ if __name__ == '__main__':
     #training(dataLoader_train, dataLoader_validation,device)
     # Test data    
     print("Calculating accuracy for the trained model on validation set ")
-    path = '/data/sawasthi/data/CAD60/validationData_pose/'
+    path = '/data/sawasthi/data/CAD60/testData_pose/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
     test_dataset = CustomDataSet(path)
