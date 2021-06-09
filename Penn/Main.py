1bccdebbd9b5e468b3a9496effc29227ed5c4278
shrutarv <shrutarv.awasthi@gmail.com>
2021-06-09 09:14:31 +0200

    Update Main.py

--- Penn/Main.py
+++ Penn/Main.py
@@ -324,7 +324,7 @@ def training(dataLoader_train, dataLoader_validation, device):
     plt.plot(ep,accuracy,'r',label='training accuracy')
     plt.plot(ep,validation_acc, 'g',label='validation accuracy')
     plt.legend()
-    plt.savefig('/data/sawasthi/data/Penn/results/result_pose.png') 
+    plt.savefig('/data/sawasthi/Penn/results/result_pose.png') 
     #plt.savefig('S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/result.png') 
     #plt.savefig('S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/result.png'
 
@@ -541,11 +541,11 @@ if __name__ == '__main__':
     #optimizer = optim.Adam(model.parameters(), lr=0.001)
     optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, alpha=0.9,weight_decay=0.0005, momentum=0.9)
     #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
-    model_path = '/data/sawasthi/data/Penn/model/model_acc_down2.pth'
+    model_path = '/data/sawasthi/Penn/model/model_acc_down2.pth'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
    
-    path = '/data/sawasthi/data/Penn/trainData_opp_1/'
+    path = '/data/sawasthi/Penn/trainData_opp_1/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
@@ -558,7 +558,7 @@ if __name__ == '__main__':
   
     
     # Validation data    
-    path = '/data/sawasthi/data/Penn/validationtData_opp_1/'
+    path = '/data/sawasthi/Penn/validationtData_opp_1/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
@@ -573,7 +573,7 @@ if __name__ == '__main__':
     training(dataLoader_train, dataLoader_validation,device)
     # Test data    
     print("Calculating accuracy for the trained model on validation set ")
-    path = '/data/sawasthi/data/Penn/testData_opp_1/'
+    path = '/data/sawasthi/Penn/testData_opp_1/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
     test_dataset = CustomDataSet(path)
