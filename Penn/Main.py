f5a61fa975aeae790725db2111f62924df7e18f6
shrutarv <shrutarv.awasthi@gmail.com>
2021-06-09 10:59:05 +0200

    Update Main.py

--- Penn/Main.py
+++ Penn/Main.py
@@ -541,11 +541,11 @@ if __name__ == '__main__':
     #optimizer = optim.Adam(model.parameters(), lr=0.001)
     optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, alpha=0.9,weight_decay=0.0005, momentum=0.9)
     #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
-    model_path = '/data/sawasthi/data/Penn/model/model_acc_up1.pth'
+    model_path = '/data/sawasthi/data/Penn/model/model_pose_1.pth'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
    
-    path = '/data/sawasthi/data/Penn/trainData_acc_up1/'
+    path = '/data/sawasthi/data/Penn/trainData_pose_1/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
@@ -558,7 +558,7 @@ if __name__ == '__main__':
   
     
     # Validation data    
-    path = '/data/sawasthi/data/Penn/validationData_acc_up1/'
+    path = '/data/sawasthi/data/Penn/validationData_pose_1/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
@@ -573,7 +573,7 @@ if __name__ == '__main__':
     #training(dataLoader_train, dataLoader_validation,device)
     # Test data    
     print("Calculating accuracy for the trained model on validation set ")
-    path = '/data/sawasthi/data/Penn/testData_acc_up1/'
+    path = '/data/sawasthi/data/Penn/testData_pose_1/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
     test_dataset = CustomDataSet(path)
