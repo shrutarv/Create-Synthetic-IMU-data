f9b9432711247702a8a5178f9f418257c53af5e0
shrutarv <shrutarv.awasthi@gmail.com>
2021-06-09 09:49:22 +0200

    Update Main.py

--- JHMDB/Main.py
+++ JHMDB/Main.py
@@ -570,7 +570,7 @@ if __name__ == '__main__':
     device = torch.device(dev)
     config = {
         "NB_sensor_channels":30,
-        "sliding_window_length":100,
+        "sliding_window_length":25,
         "filter_size":5,
         "num_filters":64,
         "network":"cnn",
