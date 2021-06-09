cdf13d0505e2774e520daa4834e9b85c5282d172
shrutarv <shrutarv.awasthi@gmail.com>
2021-06-09 09:24:24 +0200

    Update Main.py

--- JHMDB/Main.py
+++ JHMDB/Main.py
@@ -578,7 +578,7 @@ if __name__ == '__main__':
         "num_classes":21,
         "reshape_input":False,
         "step_size":12,
-        "model_path": '/data/sawasthi/JHMDB/model/model_pose.pth'
+        "model_path": '/data/sawasthi/JHMDB/model/model_acc_up1.pth'
         }
 
 
