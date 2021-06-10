50e0899be4be5620bb2affb2fdcf9b4cb8d15a84
shrutarv <shrutarv.awasthi@gmail.com>
2021-06-10 16:22:35 +0200

    Update Main.py

--- PAMAP2/Main.py
+++ PAMAP2/Main.py
@@ -364,7 +364,7 @@ def testing(config):
 if __name__ == '__main__':
     
     if torch.cuda.is_available():  
-          dev = "cuda:2" 
+          dev = "cuda:0" 
     else:  
           dev = "cpu"  
           
