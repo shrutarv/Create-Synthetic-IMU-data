646424908dea779ab54aa1aa432f40d4dfa88ec4
shrutarv <shrutarv.awasthi@gmail.com>
2021-06-11 12:55:44 +0200

    Update Main_CAD2.py

--- transferLearningLaraIMU/Main_CAD2.py
+++ transferLearningLaraIMU/Main_CAD2.py
@@ -472,7 +472,7 @@ if __name__ == '__main__':
         
         ws=100
         accumulation_steps = 5
-        epochs = 64
+        epochs = 30
         batch_size = 100
         learning_rate = 0.00001
         print("sliding_window_length", config["sliding_window_length"],"epoch: ",epochs,"batch_size: ",batch_size,"accumulation steps: ",accumulation_steps,"ws: ",ws, "learning_rate: ",learning_rate)
@@ -517,7 +517,7 @@ if __name__ == '__main__':
         #optimizer = optim.Adam(model.parameters(), lr=0.001)
         optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, alpha=0.9,weight_decay=0.0005, momentum=0.9)
         optimizer.zero_grad()
-        path = '/data/sawasthi/LaraIMU/trainData_75_new/'
+        path = '/data/sawasthi/LaraIMU/trainData_30_new/'
         #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
         #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
@@ -557,7 +557,7 @@ if __name__ == '__main__':
             data_x.to(device)
             value = max_min_values(data_x,value)
         '''
-        model_path_tl = '/data/sawasthi/LaraIMU/model/model_tl_CAD_c1_75_new.pth'
+        model_path_tl = '/data/sawasthi/LaraIMU/model/model_tl_CAD_c1_30_new.pth'
         
         training(dataLoader_train, dataLoader_validation,device,flag)
         WF, TA = testing(config)
