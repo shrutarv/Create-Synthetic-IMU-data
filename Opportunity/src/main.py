5383f796f0001a7af1cbd62bc4c2ba46204eb128
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-03 23:08:01 +0200

    NTU

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -233,7 +233,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                      'freeze_options': freeze_options[freeze],
                      'proportions': proportions[proportions_id],
                      'fully_convolutional': fully_convolutional,
-                     'model_path': '/data/sawasthi/Opportunity/model/network_NTU_ges_c1_pose.pt'}
+                     'model_path': '/data/sawasthi/Opportunity/model/network_NTU_ges_c1_c2_c3_pose.pt'}
     
     return configuration
 
@@ -339,7 +339,7 @@ def gestures_main():
                         config = configuration(dataset_idx=1, network_idx=arch, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=0, name_counter=0,
                                                freeze=1, proportions_id = 3, gpudevice = "0")
-                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_NTU_ges_c1_pose.txt")
+                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_NTU_ges_c1_c2_c3_pose.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
