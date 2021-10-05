924d75ca3cc0044800878f519f38de922924107e
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-05 22:31:09 +0200

    Update main.py

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -233,7 +233,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                      'freeze_options': freeze_options[freeze],
                      'proportions': proportions[proportions_id],
                      'fully_convolutional': fully_convolutional,
-                     'model_path': '/data/sawasthi/Opportunity/model/network_NTU_ges_c1_acc_f_75.pt'}
+                     'model_path': '/data/sawasthi/Opportunity/model/network_NTU_ges_c1_acc_f_50.pt'}
     
     return configuration
 
@@ -338,8 +338,8 @@ def gestures_main():
                     for pp in proportions_opts:
                         config = configuration(dataset_idx=1, network_idx=arch, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=0, name_counter=0,
-                                               freeze=1, proportions_id = 2, gpudevice = "0")
-                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_NTU_ges_c1_acc_f_75.txt")
+                                               freeze=1, proportions_id = 1, gpudevice = "0")
+                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_NTU_ges_c1_acc_f_50.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
