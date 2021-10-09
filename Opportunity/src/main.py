1e16c9710c51cce3bdf706262fcd1f97bc455f8d
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-09 23:18:50 +0200

    Update main.py

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -233,7 +233,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                      'freeze_options': freeze_options[freeze],
                      'proportions': proportions[proportions_id],
                      'fully_convolutional': fully_convolutional,
-                     'model_path': '/data/sawasthi/Opportunity/model/network_NTU_ges_c1_c2_c3_acc_nf.pt'}
+                     'model_path': '/data/sawasthi/Opportunity/model/network_NTU_loc_c1_c2_c3_c4_acc.pt'}
     
     return configuration
 
@@ -312,8 +312,8 @@ def locomotion_main():
                     for pp in proportions_opts:
                         config = configuration(dataset_idx=dset, network_idx=arch, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=0, name_counter=0,
-                                           freeze=1, proportions_id = 2, gpudevice = "0")
-                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_JHMDB_loc_c1_c2_c3_pose.txt")
+                                           freeze=1, proportions_id = 3, gpudevice = "0")
+                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_NTU_loc_c1_c2_c3_c4_acc.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
@@ -377,7 +377,7 @@ if __name__ == '__main__':
     
     
     #pamap2_main()
-    #locomotion_main()
-    gestures_main()
+    locomotion_main()
+    #gestures_main()
     
     print("Done")
