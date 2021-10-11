1df813b7fe8a4815c8af82c09648c14f2804ba60
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-11 22:29:50 +0200

    Update main.py

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -118,7 +118,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
     # Folder
     if usage_modus[usage_modus_idx] == 'train':
         folder_exp = '/data/sawasthi/Opportunity/model/'
-        folder_exp_base_fine_tuning = '/data/sawasthi/NTU/model/model_up1_3a.pt' #model_up1_3a.pt
+        folder_exp_base_fine_tuning = '/data/sawasthi/NTU/model/model_3.pt' #model_up1_3a.pt
         '''
         folder_exp = '/data/fmoya/HAR/pytorch/' + dataset[dataset_idx] + '/' + \
                      network[network_idx] + '/' + output[output_idx] + '/' + fully_convolutional + '/' \
@@ -233,7 +233,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                      'freeze_options': freeze_options[freeze],
                      'proportions': proportions[proportions_id],
                      'fully_convolutional': fully_convolutional,
-                     'model_path': '/data/sawasthi/Opportunity/model/network_NTU_ges_acc_c1_f_50.pt'}
+                     'model_path': '/data/sawasthi/Opportunity/model/network_NTU_loc_pose_c1_f.pt'}
     
     return configuration
 
@@ -312,8 +312,8 @@ def locomotion_main():
                     for pp in proportions_opts:
                         config = configuration(dataset_idx=dset, network_idx=arch, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=0, name_counter=0,
-                                           freeze=1, proportions_id = 1, gpudevice = "0")
-                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_NTU_loc_acc_c1_f_50.txt")
+                                           freeze=1, proportions_id = 3, gpudevice = "0")
+                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_NTU_loc_pose_c1_f.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
