2b4ae88662e82d15587f06eca9b0f124054b2058
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-12 16:26:22 +0200

    Update Main.py

--- NTU/Main.py
+++ NTU/Main.py
@@ -575,7 +575,7 @@ if __name__ == '__main__':
         "reshape_input":False,
         "step_size":3,
         "device": "cuda:0",
-        "model_path": '/data/sawasthi/NTU/model/model_up1_3a.pt'
+        "model_path": '/data/sawasthi/NTU/model/model_up3_3a.pt'
         }
 
     if torch.cuda.is_available():  
@@ -616,7 +616,7 @@ if __name__ == '__main__':
     
     #model_path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/model.pth'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
-    path = '/data/sawasthi/NTU/trainData_up_1a/'
+    path = '/data/sawasthi/NTU/trainData_up_3a/'
     #path = 'S:/Datasets/nturgbd_skeletons_s001_to_s017/train/'
     #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
@@ -630,7 +630,7 @@ if __name__ == '__main__':
    
     # Validation data    
     #path = 'S:/Datasets/nturgbd_skeletons_s001_to_s017/val/'
-    path = '/data/sawasthi/NTU/validationData_up_1a/'
+    path = '/data/sawasthi/NTU/validationData_up_3a/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
     validation_dataset = CustomDataSet(path)
@@ -649,7 +649,7 @@ if __name__ == '__main__':
     '''
     #training(dataLoader_train, dataLoader_validation,device, config)
     print("Calculating accuracy for the trained model on validation set ")
-    path = '/data/sawasthi/NTU/validationData_up_1a/'
+    path = '/data/sawasthi/NTU/validationData_up_3a/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
@@ -661,7 +661,7 @@ if __name__ == '__main__':
                                    drop_last=True)
     
     testing(config)
-    path = '/data/sawasthi/NTU/testData_up_1a/'
+    path = '/data/sawasthi/NTU/testData_up_3a/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/Test_pkl/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
     test_dataset = CustomDataSet(path)
