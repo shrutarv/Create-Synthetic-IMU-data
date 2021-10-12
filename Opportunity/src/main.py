d4612cce67af5a1b81e695918c1603fa5d020130
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-12 14:20:11 +0200

    Update main.py

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -233,7 +233,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                      'freeze_options': freeze_options[freeze],
                      'proportions': proportions[proportions_id],
                      'fully_convolutional': fully_convolutional,
-                     'model_path': '/data/sawasthi/Opportunity/model/network_NTU_loc_pose_c1_f_75.pt'}
+                     'model_path': '/data/sawasthi/Opportunity/model/network_NTU_loc_pose_c1_f_50.pt'}
     
     return configuration
 
@@ -312,8 +312,8 @@ def locomotion_main():
                     for pp in proportions_opts:
                         config = configuration(dataset_idx=dset, network_idx=arch, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=0, name_counter=0,
-                                           freeze=1, proportions_id = 2, gpudevice = "0")
-                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_NTU_loc_pose_c1_f_75.txt")
+                                           freeze=1, proportions_id = 1, gpudevice = "0")
+                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_NTU_loc_pose_c1_f_50.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
