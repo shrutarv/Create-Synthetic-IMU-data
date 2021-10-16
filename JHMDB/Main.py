401ecf6c2875064ef4084872b3a61657fd3ac775
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-16 17:42:31 +0200

    Update Main.py

--- JHMDB/Main.py
+++ JHMDB/Main.py
@@ -578,7 +578,7 @@ if __name__ == '__main__':
         "num_classes":21,
         "reshape_input":False,
         "step_size":12,
-        "model_path": '/data/sawasthi/data/JHMDB/model/model_pose_12.pth'
+        "model_path": '/data/sawasthi/JHMDB/model/model_pose_12.pth'
         }
 
 
@@ -613,7 +613,7 @@ if __name__ == '__main__':
     #model_path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/model.pth'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
 
-    path = '/data/sawasthi/data/JHMDB/trainData_pose_12/'
+    path = '/data/sawasthi/JHMDB/trainData_pose_12/'
 
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
@@ -627,7 +627,7 @@ if __name__ == '__main__':
   
    
     # Validation data    
-    path = '/data/sawasthi/data/JHMDB/validationData_pose_12/'
+    path = '/data/sawasthi/JHMDB/validationData_pose_12/'
 
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
@@ -648,7 +648,7 @@ if __name__ == '__main__':
     '''
     #training(dataLoader_train, dataLoader_validation,device, config)
     print("Calculating accuracy for the trained model on validation set ")
-    path = '/data/sawasthi/data/JHMDB/testData_pose_12/'
+    path = '/data/sawasthi/JHMDB/testData_pose_12/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
