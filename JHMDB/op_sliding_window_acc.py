47c0249d6f77ef2e505eee9622a90cc9508ff2b7
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-16 18:32:53 +0200

    Update op_sliding_window_acc.py

--- JHMDB/op_sliding_window_acc.py
+++ JHMDB/op_sliding_window_acc.py
@@ -204,7 +204,7 @@ if __name__ == '__main__':
     # The training, test and validation data have been separately interpolated and 
     # up sampled
     # up sampling rate
-    up = 1
+    up = 4
     #ws = (100,31)
     ws = (25,30) 
     ss = (12,30)     
@@ -283,7 +283,7 @@ if __name__ == '__main__':
     
     #data_dir = "/media/shrutarv/Drive1/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows2/"
     #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/train_data25_39.csv')
-    data_dir =  '/data/sawasthi/JHMDB/trainData_acc_up1/'
+    data_dir =  '/data/sawasthi/JHMDB/trainData_acc_up4/'
     #data_dir = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     label = np.repeat(data[:,31],up).astype(int)
     lab = np.zeros((len(label),20), dtype=int)
@@ -295,7 +295,7 @@ if __name__ == '__main__':
     print("train data pickled")
     
     #data_dir = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl'
-    data_dir =  '/data/sawasthi/JHMDB/testData_acc_up1/'
+    data_dir =  '/data/sawasthi/JHMDB/testData_acc_up4/'
     df = pd.read_csv('/home/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/test_data.csv')
     data = df.values
     data = normalize(data,value, "test")
@@ -327,7 +327,7 @@ if __name__ == '__main__':
     example_creating_windows_file(k, X, lab, data_dir)
     print("test data pickled")
     
-    data_dir =  '/data/sawasthi/JHMDB/validationData_acc_up1/'
+    data_dir =  '/data/sawasthi/JHMDB/validationData_acc_up4/'
     #data_dir =  '/data/sawasthi/data/JHMDB/validationData/'
     df = pd.read_csv('/home/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/validation_data.csv')
     data = df.values
