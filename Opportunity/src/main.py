c6910068ac9f8aff70ee5f3bdbb1c2229ca9403c
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-17 00:02:35 +0200

    Update main.py

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -118,7 +118,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
     # Folder
     if usage_modus[usage_modus_idx] == 'train':
         folder_exp = '/data/sawasthi/PAMAP2/model/'
-        folder_exp_base_fine_tuning = '/data/sawasthi/JHMDB/model/model_acc_up4.pt' #model_up1_3a.pt
+        folder_exp_base_fine_tuning = '/data/sawasthi/JHMDB/model/model_acc_u' #model_up1_3a.pt
         '''
         folder_exp = '/data/fmoya/HAR/pytorch/' + dataset[dataset_idx] + '/' + \
                      network[network_idx] + '/' + output[output_idx] + '/' + fully_convolutional + '/' \
