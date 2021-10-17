418627c60109f235b34d88fd9a3af827e5e5ad69
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-18 00:12:21 +0200

    Update main.py

--- CAD60/main.py
+++ CAD60/main.py
@@ -542,11 +542,11 @@ if __name__ == '__main__':
     #optimizer = optim.Adam(model.parameters(), lr=0.001)
     optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, alpha=0.9,weight_decay=0.0005, momentum=0.9)
     #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
-    model_path = '/data/sawasthi/data/CAD60/model/model_acc_up3.pth'
+    model_path = '/data/sawasthi/data/CAD60/model/model_acc_up3_tf.pth'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
    
-    path = '/data/sawasthi/data/CAD60/trainData_acc_up3/'
+    path = '/data/sawasthi/data/CAD60/trainData_acc_up3_tf/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
