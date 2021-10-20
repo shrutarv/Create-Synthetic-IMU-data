80807798d96a5c602b8e0008486521f338d34b69
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-20 10:39:29 +0200

    Update main.py

--- CAD60/main.py
+++ CAD60/main.py
@@ -336,7 +336,8 @@ def testing(config):
     correct = 0.0
     trueValue = np.array([], dtype=np.int64)
     prediction = np.array([], dtype=np.int64)
-    model = torch.load(model_path)
+    #model = torch.load(model_path)
+    torch.load(config['model_path'], map_location=torch.device('cpu'))['state_dict']
     model.eval()
     model.to(device)
     loss_test = 0.0
@@ -571,7 +572,7 @@ if __name__ == '__main__':
                                    drop_last=True)
     
    
-    training(dataLoader_train, dataLoader_validation,device)
+    #training(dataLoader_train, dataLoader_validation,device)
     # Test data    
     print("Calculating accuracy for the trained model on validation set ")
     path = '/data/sawasthi/data/CAD60/validationData_acc_up3/'
