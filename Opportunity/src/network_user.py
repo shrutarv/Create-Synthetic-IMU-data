4e7d489b341956e5e1c364939d2334ede47d1a96
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-21 08:38:54 +0200

    cpnf

--- Opportunity/src/network_user.py
+++ Opportunity/src/network_user.py
@@ -340,8 +340,8 @@ class Network_User(object):
         #    print(k)
 
         if self.config["network"] == 'cnn':
-            list_layers = ['conv1_1.weight', 'conv1_1.bias', 'conv1_2.weight', 'conv1_2.bias'
-                         
+            list_layers = ['conv1_1.weight', 'conv1_1.bias', 'conv1_2.weight', 'conv1_2.bias',
+                           'conv2_1.weight', 'conv2_1.bias'
                            ]
         elif self.config["network"] == 'cnn_imu':
             list_layers = ['conv_LA_1_1.weight', 'conv_LA_1_1.bias', 'conv_LA_1_2.weight', 'conv_LA_1_2.bias',
@@ -380,8 +380,8 @@ class Network_User(object):
         logging.info('        Network_User:        Setting Required_grad to Weights')
 
         if self.config["network"] == 'cnn':
-            list_layers = ['conv1_1.weight', 'conv1_1.bias', 'conv1_2.weight', 'conv1_2.bias'
-                           
+            list_layers = ['conv1_1.weight', 'conv1_1.bias', 'conv1_2.weight', 'conv1_2.bias',
+                           'conv2_1.weight', 'conv2_1.bias'
                           ]
         elif self.config["network"] == 'cnn_imu':
             list_layers = ['conv_LA_1_1.weight', 'conv_LA_1_1.bias', 'conv_LA_1_2.weight', 'conv_LA_1_2.bias',
