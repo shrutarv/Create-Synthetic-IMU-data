751d966e73dc5e9e95e82468290d808138f6b872
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-10-27 05:20:41 +0200

    Update Main_CAD2.py

--- transferLearningLaraIMU/Main_CAD2.py
+++ transferLearningLaraIMU/Main_CAD2.py
@@ -465,6 +465,7 @@ if __name__ == '__main__':
         #"folder_exp_base_fine_tuning": 'S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/model_full.pth'
         "freeze":True
         }
+    
     flag = True
     iterations = 3
     weighted_F1_array = []
