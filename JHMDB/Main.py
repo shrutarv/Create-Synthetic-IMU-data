0a8b54396ce66d8fc5fd039784b3d41c5c4151f3
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-11-13 10:31:25 +0100

    Update Main.py

--- JHMDB/Main.py
+++ JHMDB/Main.py
@@ -578,7 +578,7 @@ if __name__ == '__main__':
         "num_classes":21,
         "reshape_input":False,
         "step_size":12,
-        "model_path": '/data/sawasthi/JHMDB/model/model_pose_ci_tf.pth',
+        "model_path": '/data/sawasthi/JHMDB/model/model_acc_ci_tf.pth',
         "dataset" : 'JHMDB'
         
         }
@@ -615,7 +615,7 @@ if __name__ == '__main__':
     #model_path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/model.pth'
     #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
 
-    path = '/data/sawasthi/JHMDB/trainData_pose_12/'
+    path = '/data/sawasthi/JHMDB/trainData_acc_up4/' #trainData_pose_12
     #path = 'S:/Datasets/JHMDB/train/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
@@ -629,7 +629,7 @@ if __name__ == '__main__':
   
    
     # Validation data    
-    path = '/data/sawasthi/JHMDB/validationData_pose_12/'
+    path = '/data/sawasthi/JHMDB/validationData_acc_up4/'
 
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
@@ -650,7 +650,7 @@ if __name__ == '__main__':
     '''
     training(dataLoader_train, dataLoader_validation,device, config)
     print("Calculating accuracy for the trained model on validation set ")
-    path = '/data/sawasthi/JHMDB/validationData_pose_12/'
+    path = '/data/sawasthi/JHMDB/validationData_acc_up4/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
     #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
@@ -662,7 +662,7 @@ if __name__ == '__main__':
                                    drop_last=True)
     
     testing(config)
-    path = '/data/sawasthi/JHMDB/testData_pose_12/'
+    path = '/data/sawasthi/JHMDB/testData_acc_up4/'
     #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/Test_pkl/'
     #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
     test_dataset = CustomDataSet(path)
