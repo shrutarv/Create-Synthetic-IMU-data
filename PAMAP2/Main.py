cf118c7f172fe434d62db26b0b81c95302abab02
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-11-19 16:44:20 +0100

    Update Main.py

--- PAMAP2/Main.py
+++ PAMAP2/Main.py
@@ -500,7 +500,7 @@ if __name__ == '__main__':
 
         ws=100
         accumulation_steps = 10
-        epochs = 80
+        epochs = 100
         batch_size = 200
         learning_rate = 0.00001
               
