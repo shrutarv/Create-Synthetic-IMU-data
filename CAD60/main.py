b9ccb3ffcac0c5f74c28069f6d3dbc169c50e841
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-11-21 09:36:33 +0100

    Update main.py

--- CAD60/main.py
+++ CAD60/main.py
@@ -547,7 +547,7 @@ if __name__ == '__main__':
         #model_path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
         #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
        
-        path = '/data/sawasthi/CAD60/trainData_pose_tf/'
+        path = '/data/sawasthi/CAD60/trainData_ss_12/'
         #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
         #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
@@ -560,7 +560,7 @@ if __name__ == '__main__':
       
         
         # Validation data    
-        path = '/data/sawasthi/CAD60/validationData_pose/'
+        path = '/data/sawasthi/CAD60/validationData_ss_12/'
         #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
         #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
@@ -575,7 +575,7 @@ if __name__ == '__main__':
         training(dataLoader_train, dataLoader_validation,device,flag)
         # Test data    
         print("Calculating accuracy for the trained model on validation set ")
-        path = '/data/sawasthi/CAD60/validationData_pose/'
+        path = '/data/sawasthi/CAD60/validationData_ss_12/'
         #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
         test_dataset = CustomDataSet(path)
@@ -587,7 +587,7 @@ if __name__ == '__main__':
         
         #testing(config)
         # Test data    
-        path = '/data/sawasthi/CAD60/testData_pose/'
+        path = '/data/sawasthi/CAD60/testData_ss_12/'
         #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
         test_dataset = CustomDataSet(path)
