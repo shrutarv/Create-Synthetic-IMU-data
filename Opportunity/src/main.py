e770d0d8d5011e1752a2fd7d02dbb3f5fff6f359
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-11-24 10:06:40 +0100

    c2

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -233,7 +233,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                      'freeze_options': freeze_options[freeze],
                      'proportions': proportions[proportions_id],
                      'fully_convolutional': fully_convolutional,
-                     'model_path': '/data/sawasthi/Opportunity/model/network_CAD60_PAMAP2_cimu_pose_nf_c1.pt'}
+                     'model_path': '/data/sawasthi/Opportunity/model/network_CAD60_PAMAP2_cimu_pose_nf_c1_c2.pt'}
     
     return configuration
 
@@ -286,7 +286,7 @@ def pamap2_main():
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=0, name_counter=0,
                                                freeze=0, proportions_id=3, gpudevice="0")
                         setup_experiment_logger(logging_level=logging.DEBUG,
-                                                filename=config['folder_exp'] + "logger_CAD60_PAMAP2_cimu_pose_nf_c1.txt")
+                                                filename=config['folder_exp'] + "logger_CAD60_PAMAP2_cimu_pose_nf_c1_c2.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
