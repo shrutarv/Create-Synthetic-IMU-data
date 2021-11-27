6c74c39eb17501a103b16ef8c76fb6eabc652e01
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-11-27 10:37:00 +0100

    c1

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -313,7 +313,7 @@ def locomotion_main():
                         config = configuration(dataset_idx=dset, network_idx=arch, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=1, name_counter=0,
                                            freeze=1, proportions_id = 3, gpudevice = "0")
-                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_CAD60_loc_pose_c1_f_50.txt")
+                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_CAD60_loc_acc_c1_nf.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
