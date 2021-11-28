9b9191f9ec1e4804c64f696051e907abc1803f02
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-11-28 19:29:52 +0100

    Update main.py

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -233,7 +233,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                      'freeze_options': freeze_options[freeze],
                      'proportions': proportions[proportions_id],
                      'fully_convolutional': fully_convolutional,
-                     'model_path': '/data/sawasthi/Opportunity/model/network_CAD60_loc_cimu_pose_nf_c1_30.pt'}
+                     'model_path': '/data/sawasthi/Opportunity/model/network_CAD60_loc_cimu_pose_nf_c1_75.pt'}
     
     return configuration
 
@@ -312,8 +312,8 @@ def locomotion_main():
                     for pp in proportions_opts:
                         config = configuration(dataset_idx=dset, network_idx=arch, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=1, name_counter=0,
-                                           freeze=0, proportions_id = 0, gpudevice = "0")
-                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_CAD60_loc_pose_c1_30_nf.txt")
+                                           freeze=0, proportions_id = 2, gpudevice = "0")
+                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_CAD60_loc_pose_c1_75_nf.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
