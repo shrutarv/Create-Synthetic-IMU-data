1de07fdf7fde8861c3d4e17660a02dd80cefd175
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-11-29 13:57:18 +0100

    Update Main.py

--- transferLearningPAMAP2/Main.py
+++ transferLearningPAMAP2/Main.py
@@ -503,7 +503,7 @@ if __name__ == '__main__':
         #optimizer = optim.Adam(model.parameters(), lr=0.001)
         optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, alpha=0.9,weight_decay=0.0005, momentum=0.9)
         #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
-        path = '/data/sawasthi/PAMAP2/trainData_30per/'
+        path = '/data/sawasthi/PAMAP2/trainData_30_new/'
         #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
         #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
