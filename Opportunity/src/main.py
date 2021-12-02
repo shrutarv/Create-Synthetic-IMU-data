a6de390f72dba73d4a42913e267ae4bc143469e0
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-12-02 16:37:13 +0100

    Update main.py

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -151,7 +151,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
         '''
     elif usage_modus[usage_modus_idx] == 'fine_tuning':
         folder_exp = '/data/sawasthi/PAMAP2/model/'
-        folder_exp_base_fine_tuning = '/data/sawasthi/CAD60/model/model_acc_ci_up3_tf.pth' #model_acc_up4.pth #model_up1_3a.pt
+        folder_exp_base_fine_tuning = '/data/sawasthi/CAD60/model/model_acc_ci_up1_tf.pth' #model_acc_up4.pth #model_up1_3a.pt
         '''
         folder_exp = '/data2/fmoya/HAR/pytorch/' + dataset[dataset_idx] + '/' + \
                      network[network_idx] + '/' + output[output_idx] + '/' + fully_convolutional + \
@@ -233,7 +233,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                      'freeze_options': freeze_options[freeze],
                      'proportions': proportions[proportions_id],
                      'fully_convolutional': fully_convolutional,
-                     'model_path': '/data/sawasthi/Opportunity/model/network_CAD60_loc_cimu_pose_nf_c1_50.pt'}
+                     'model_path': '/data/sawasthi/Opportunity/model/network_CAD60_ges_ci_acc_nf_c1_.pt'}
     
     return configuration
 
@@ -310,10 +310,10 @@ def locomotion_main():
             for arch in networks_arc:
                 for fopt in frezze_opts:
                     for pp in proportions_opts:
-                        config = configuration(dataset_idx=dset, network_idx=arch, output_idx=0, usage_modus_idx=5,
+                        config = configuration(dataset_idx=dset, network_idx=2, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=1, name_counter=0,
                                            freeze=0, proportions_id = 1, gpudevice = "0")
-                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_CAD60_loc_pose_c1_50_nf.txt")
+                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_JHMDB_cnn_loc_acc_c1_c2_c3_c4_nf.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
@@ -336,9 +336,9 @@ def gestures_main():
             for arch in networks_arc:
                 for fopt in frezze_opts:
                     for pp in proportions_opts:
-                        config = configuration(dataset_idx=1, network_idx=arch, output_idx=0, usage_modus_idx=5,
+                        config = configuration(dataset_idx=1, network_idx=2, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=1, name_counter=0,
-                                               freeze=0, proportions_id = 0, gpudevice = "0")
+                                               freeze=0, proportions_id = 0, gpudevice = "1")
                         setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_CAD60_ges_c1_acc_nf.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
@@ -377,7 +377,7 @@ if __name__ == '__main__':
     
     
     #pamap2_main()
-    locomotion_main()
-    #gestures_main()
+    #locomotion_main()
+    gestures_main()
     
     print("Done")
