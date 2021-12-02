2b2c2841918b3ce105d0b712dae535266395efa7
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-12-02 16:41:34 +0100

    Update main.py

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -338,7 +338,7 @@ def gestures_main():
                     for pp in proportions_opts:
                         config = configuration(dataset_idx=1, network_idx=2, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=1, name_counter=0,
-                                               freeze=0, proportions_id = 0, gpudevice = "1")
+                                               freeze=0, proportions_id = 0, gpudevice = "0")
                         setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_CAD60_ges_c1_acc_nf.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
