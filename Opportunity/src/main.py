0ed75dfe1678e5469c8f901fde4aef2d2e1c3f1c
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-12-09 12:22:48 +0100

    c4

--- Opportunity/src/main.py
+++ Opportunity/src/main.py
@@ -233,7 +233,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                      'freeze_options': freeze_options[freeze],
                      'proportions': proportions[proportions_id],
                      'fully_convolutional': fully_convolutional,
-                     'model_path': '/data/sawasthi/Opportunity/model/network_CAD60_ges_cnn_c1_pose_nf.pt'}
+                     'model_path': '/data/sawasthi/Opportunity/model/network_CAD60_ges_cnn_c4_pose_nf.pt'}
     
     return configuration
 
@@ -339,7 +339,7 @@ def gestures_main():
                         config = configuration(dataset_idx=1, network_idx=0, output_idx=0, usage_modus_idx=5,
                                                dataset_fine_tuning_idx=ft, learning_rates_idx=1, name_counter=0,
                                                freeze=0, proportions_id = 4, gpudevice = "0")
-                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_CAD60_ges_c1_pose_nf.txt")
+                        setup_experiment_logger(logging_level=logging.DEBUG, filename= config['folder_exp'] + "logger_CAD60_ges_c4_pose_nf.txt")
                         logging.info('Finished')
                         modus = Modus_Selecter(config)
                         #Starting process
