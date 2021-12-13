2dfbc00f56ec0bad162f31ce90510ac95de64992
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-12-13 22:42:26 +0100

    Update Main.py

--- NTU/Main.py
+++ NTU/Main.py
@@ -578,7 +578,7 @@ if __name__ == '__main__':
         "step_size":3,
         "device": "cuda:0",
         "model_path": '/data/sawasthi/NTU/model/model_up1_acc.pt',
-        "dataset":"CAD60"
+        "dataset":"NTU"
         }
 
     if torch.cuda.is_available():  
