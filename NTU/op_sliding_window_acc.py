45318340db03998eb3c524a4802f04cde660c950
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-12-15 10:23:41 +0100

    Update op_sliding_window_acc.py

--- NTU/op_sliding_window_acc.py
+++ NTU/op_sliding_window_acc.py
@@ -214,8 +214,8 @@ if __name__ == '__main__':
     #sliding_window_length = 100    
     sliding_window_step = 3
     
-    df = pd.read_csv('/data/sawasthi/NTU/train_data_tf_new.csv')
-    #df = pd.read_csv('S:/Datasets/nturgbd_skeletons_s001_to_s017/train_data_tf_new.csv')
+    df = pd.read_csv('/data/sawasthi/NTU/train_data_new.csv')
+    #df = pd.read_csv('S:/Datasets/nturgbd_skeletons_s001_to_s017/train_data_new.csv')
     data = df.values
     attr = np.zeros((30,1))
     #value = pd.read_csv('S:/Datasets/nturgbd_skeletons_s001_to_s017/norm_values_tf.csv')
@@ -224,10 +224,10 @@ if __name__ == '__main__':
     value = value.values
     '''
     value = max_min_values(data)
-    with open("S:/Datasets/nturgbd_skeletons_s001_to_s017/norm_values_tf.csv", 'w') as f:
+    with open("S:/Datasets/nturgbd_skeletons_s001_to_s017/norm_values.csv", 'w') as f:
         fc = csv.writer(f, lineterminator='\n')
         fc.writerow(["min","max"])
-        fc.writerows(value)
+        fc.writerows(value2)
     plt.plot(data[:,0],data[:,1])
     '''
     
@@ -287,7 +287,7 @@ if __name__ == '__main__':
     
     #data_dir = "/media/shrutarv/Drive1/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows2/"
     #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/train_data25_39.csv')
-    data_dir =  '/data/sawasthi/NTU/trainData_tf_1a/'
+    data_dir =  '/data/sawasthi/NTU/trainData_up_1a/'
     #data_dir = 'S:/Datasets/nturgbd_skeletons_s001_to_s017/train/'
     label = np.repeat(data[:,76],up).astype(int)
     lab = np.zeros((len(label),20), dtype=int)
@@ -328,7 +328,7 @@ if __name__ == '__main__':
     lab[:,0] = label
     X = data_new
     k = 0
-    example_creating_windows_file(k, X, lab, data_dir)
+    #example_creating_windows_file(k, X, lab, data_dir)
     print("test data pickled")
     
     data_dir =  '/data/sawasthi/NTU/validationData_up_1a/'
@@ -361,7 +361,7 @@ if __name__ == '__main__':
     lab[:,0] = label
     X = data_new
     k = 0
-    example_creating_windows_file(k, X, lab, data_dir)
+    #example_creating_windows_file(k, X, lab, data_dir)
     print("validation data pickled")
     #os.chdir('/vol/actrec/DFG_Project/2019/Mbientlab/recordings_2019/07_IMU_synchronized_annotated/' + folder_name)
     #os.chdir("/vol/actrec/DFG_Project/2019/MoCap/recordings_2019/14_Annotated_Dataset/" + folder_name)
