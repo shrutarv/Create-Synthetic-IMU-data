321d26672b069ac767f55b7518a4c2c2e23615a1
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-12-18 16:10:07 +0100

    c1 50

--- NTU/Main.py
+++ NTU/Main.py
@@ -573,13 +573,13 @@ if __name__ == '__main__':
         "sliding_window_length":30,
         "filter_size":5,
         "num_filters":64,
-        "network":"cnn",
+        "network":"cnn_imu",
         "output":"softmax",
         "num_classes":60,
         "reshape_input":False,
         "step_size":3,
         "device": "cuda:0",
-        "model_path": '/data/sawasthi/NTU/model/model_cnn_pose_tf.pth',
+        "model_path": '/data/sawasthi/NTU/model/model_ci_pose_tf.pth',
         "model_complete":'/data/sawasthi/NTU/model/model_cnn_pose_tf_2.pth',
         "dataset":"NTU"
         }
