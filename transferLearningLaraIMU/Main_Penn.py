3515910b35193d0306c3e199c26c0f938679e192
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-12-20 23:12:58 +0100

    Update Main_Penn.py

--- transferLearningLaraIMU/Main_Penn.py
+++ transferLearningLaraIMU/Main_Penn.py
@@ -461,7 +461,7 @@ if __name__ == '__main__':
     # Python RNG
     np.random.seed(seed)
     random.seed(seed)
-    setup_experiment_logger(logging_level=logging.DEBUG, filename= "/data/sawasthi/LaraIMU/logger_new_c1_75.txt")
+    setup_experiment_logger(logging_level=logging.DEBUG, filename= "/data/sawasthi/LaraIMU/logger_penn_c1_30.txt")
     logging.info('Finished')
     logging.info(':Python Platform {}'.format(platform.python_version()))
      
@@ -539,7 +539,7 @@ if __name__ == '__main__':
         #optimizer = optim.Adam(model.parameters(), lr=0.001)
         optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, alpha=0.9,weight_decay=0.0005, momentum=0.9)
         optimizer.zero_grad()
-        path = '/data/sawasthi/LaraIMU/trainData_10/'
+        path = '/data/sawasthi/LaraIMU/trainData_30per/'
         #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
         #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
@@ -579,7 +579,7 @@ if __name__ == '__main__':
             data_x.to(device)
             value = max_min_values(data_x,value)
         '''
-        model_path_tl = '/data/sawasthi/LaraIMU/model/model_tl_penn_limu_c1_10_acc.pth'
+        model_path_tl = '/data/sawasthi/LaraIMU/model/model_tl_penn_limu_c1_30_acc.pth'
         
         training(dataLoader_train, dataLoader_validation,device,flag)
         WF, TA = testing(config)
