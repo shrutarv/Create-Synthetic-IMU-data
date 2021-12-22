db9e491b9ff57b498ac6120fef3bda7b490d5f84
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-12-22 07:11:25 +0100

    c1

--- transferLearningLaraIMU/Main_Penn.py
+++ transferLearningLaraIMU/Main_Penn.py
@@ -480,7 +480,7 @@ if __name__ == '__main__':
         "output":"softmax",
         "num_classes":8,
         "reshape_input":False,
-        "folder_exp_base_fine_tuning": '/data/sawasthi/Penn/model/model_acc_up2_tf_2.pth',
+        "folder_exp_base_fine_tuning": '/data/sawasthi/Penn/model/model_pose_tf.pth',
         #"folder_exp_base_fine_tuning": 'S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/model_full.pth'
         "freeze":False
         
@@ -539,7 +539,7 @@ if __name__ == '__main__':
         #optimizer = optim.Adam(model.parameters(), lr=0.001)
         optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, alpha=0.9,weight_decay=0.0005, momentum=0.9)
         optimizer.zero_grad()
-        path = '/data/sawasthi/LaraIMU/trainData_30per/'
+        path = '/data/sawasthi/LaraIMU/trainData_100/'
         #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
         #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
@@ -579,7 +579,7 @@ if __name__ == '__main__':
             data_x.to(device)
             value = max_min_values(data_x,value)
         '''
-        model_path_tl = '/data/sawasthi/LaraIMU/model/model_tl_penn_limu_c1_30_acc.pth'
+        model_path_tl = '/data/sawasthi/LaraIMU/model/model_tl_penn_limu_c1_pose.pth'
         
         training(dataLoader_train, dataLoader_validation,device,flag)
         WF, TA = testing(config)
