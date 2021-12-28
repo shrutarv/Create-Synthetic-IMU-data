3ce192ab578422cbc9af76f33f4d5dc02e0c5d4b
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-12-28 15:12:07 +0100

    Update Main.py

--- transferLearningLara_motionminers/Main.py
+++ transferLearningLara_motionminers/Main.py
@@ -504,7 +504,7 @@ if __name__ == '__main__':
         optimizer.zero_grad()
         
         #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
-        path = '/data/sawasthi/Lara_motionminer/trainData_10/'
+        path = '/data/sawasthi/data/Lara_motionminer/trainData_10/'
         #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
         #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
@@ -517,7 +517,7 @@ if __name__ == '__main__':
       
        
         # Validation data    
-        path = '/data/sawasthi/Lara_motionminer/validationData_10/'
+        path = '/data/sawasthi/data/Lara_motionminer/validationData_10/'
         #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
         #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
@@ -529,7 +529,7 @@ if __name__ == '__main__':
                                        drop_last=True)
         
         # Test data    
-        path = '/data/sawasthi/Lara_motionminer/testData_10/'
+        path = '/data/sawasthi/data/Lara_motionminer/testData_10/'
         #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
         test_dataset = CustomDataSet(path)
@@ -544,7 +544,7 @@ if __name__ == '__main__':
             data_x.to(device)
             value = max_min_values(data_x,value)
         '''
-        model_path_tl = '/data/sawasthi/Lara_motionminer/model/model_tl_JHMDB_acc_c1.pth'
+        model_path_tl = '/data/sawasthi/data/Lara_motionminer/model/model_tl_JHMDB_acc_c1.pth'
         training(dataLoader_train, dataLoader_validation,device)
         WF, TA = testing(config)
         #with open('S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/result.csv', 'w', newline='') as myfile:
