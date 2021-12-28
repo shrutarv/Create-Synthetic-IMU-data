ecf0e64dbc41521d40b3956bb3d654f83a2ee8ea
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-12-28 23:38:34 +0100

    Update Main.py

--- transferLearningLara_motionminers/Main.py
+++ transferLearningLara_motionminers/Main.py
@@ -460,7 +460,7 @@ if __name__ == '__main__':
         "output":"softmax",
         "num_classes":8,
         "reshape_input":False,
-        "folder_exp_base_fine_tuning": '/data/sawasthi/JHMDB/model/model_acc_cnn_up4_tf.pth',
+        "folder_exp_base_fine_tuning": '/data/sawasthi/JHMDB/model/model_acc_cnn_up1_tf.pth',
         #"folder_exp_base_fine_tuning": 'S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/model_full.pth'
         "freeze":False
         }
@@ -469,94 +469,108 @@ if __name__ == '__main__':
     ws=100
     accumulation_steps = 2
     epochs = 40
-    batch_size = 100
+    batch_size = 700
     learning_rate = 0.00001
     print("epoch: ",epochs,"batch_size: ",batch_size,"accumulation steps: ",accumulation_steps,"ws: ",ws, "learning_rate: ",learning_rate)
+    proportions_opts = [0, 1, 2, 3]
+    flag = True
     iterations = 3
-    weighted_F1_array = []
-    test_acc_array = []
-    for iter in range(iterations):
+    
+    for prop in proportions_opts:
+        weighted_F1_array = []
+        test_acc_array = []
+        for iter in range(iterations):
+                
+            #df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/MoCAP/norm_values.csv')
+            #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/Github/Thesis- Create Synthetic IMU data/MoCAP/norm_values.csv')
+            #value = df.values.tolist()
+            #print(len(df),len(value), len(value[0]))
+             
+            Lara_net = Network(config)
+            Lara_net.init_weights()
+            normal = torch.distributions.Normal(torch.tensor([0.0]),torch.tensor([0.001]))
+            #noise = noise.float()
             
-        #df = pd.read_csv('/data/sawasthi/Thesis--Create-Synthetic-IMU-data/MoCAP/norm_values.csv')
-        #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/Github/Thesis- Create Synthetic IMU data/MoCAP/norm_values.csv')
-        #value = df.values.tolist()
-        #print(len(df),len(value), len(value[0]))
-         
-        Lara_net = Network(config)
-        Lara_net.init_weights()
-        normal = torch.distributions.Normal(torch.tensor([0.0]),torch.tensor([0.001]))
-        #noise = noise.float()
+            criterion = nn.CrossEntropyLoss()
+            #model_path = '/data/sawasthi/data/JHMDB/model/model_tl.pth'
+            #model_path = 'S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/model.pth'
+            #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
+            #model = torch.load(model_path)
+            # transformed_net 
+            model = load_weights(Lara_net)
+            model = model.to(device)
+            print("model loaded")  
+            if config["freeze"]:
+                model = set_required_grad(model)
+            
+            #optimizer = optim.Adam(model.parameters(), lr=0.001)
+            optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, alpha=0.9,weight_decay=0.0005, momentum=0.9)
+            optimizer.zero_grad()
+            
+            #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
+            if prop==0:
+                path = '/data/sawasthi/LaraIMU/trainData_10_10/'
+            elif prop==1:
+                path = '/data/sawasthi/LaraIMU/trainData_10_30/'
+            elif prop==2:
+                path = '/data/sawasthi/LaraIMU/trainData_10_50/'
+            elif prop==3:
+                path = '/data/sawasthi/LaraIMU/trainData_10_75/'
+            elif prop==4:
+                path = '/data/sawasthi/LaraIMU/trainData_10_100/'
+            #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
+            #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
+            #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
+            train_dataset = CustomDataSet(path)
+            dataLoader_train = DataLoader(train_dataset, shuffle=True,
+                                          batch_size=batch_size,
+                                           num_workers=0,
+                                           pin_memory=True,
+                                           drop_last=True)
+          
+           
+            # Validation data    
+            path = '/data/sawasthi/data/Lara_motionminer/validationData_10/'
+            #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
+            #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
+            #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
+            validation_dataset = CustomDataSet(path)
+            dataLoader_validation = DataLoader(validation_dataset, shuffle=False,
+                                          batch_size=batch_size,
+                                           num_workers=0,
+                                           pin_memory=True,
+                                           drop_last=True)
+            
+            # Test data    
+            path = '/data/sawasthi/data/Lara_motionminer/testData_10/'
+            #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
+            #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
+            test_dataset = CustomDataSet(path)
+            dataLoader_test = DataLoader(test_dataset, shuffle=False,
+                                          batch_size=batch_size,
+                                           num_workers=0,
+                                           pin_memory=True,
+                                           drop_last=True)
+            '''
+            for b, harwindow_batched in enumerate(dataLoader_test):
+                data_x = harwindow_batched["data"]
+                data_x.to(device)
+                value = max_min_values(data_x,value)
+            '''
+            path_tl = '/data/sawasthi/LaraIMU/model/model_ntu_laraIMU_acc_ci_c1_'+str(prop)+'.pth'
+            model_path_tl = path_tl
+            training(dataLoader_train, dataLoader_validation,device)
+            WF, TA = testing(config)
+            #with open('S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/result.csv', 'w', newline='') as myfile:
+            #with open('S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/result.csv', 'w', newline='') as myfile:
+            weighted_F1_array.append(WF)
+            test_acc_array.append(TA)
+            
+        print("Mean Weighted F1 score after 5 runs is",np.mean(weighted_F1_array))
+        print("Standard deviation of Weighted F1 score after 5 runs is",np.std(weighted_F1_array))
         
-        criterion = nn.CrossEntropyLoss()
-        #model_path = '/data/sawasthi/data/JHMDB/model/model_tl.pth'
-        #model_path = 'S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/model.pth'
-        #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
-        #model = torch.load(model_path)
-        # transformed_net 
-        model = load_weights(Lara_net)
-        model = model.to(device)
-        print("model loaded")  
-        if config["freeze"]:
-            model = set_required_grad(model)
+        print("Mean Test accuracy score after 5 runs is",np.mean(test_acc_array))
+        print("Standard deviation of Test accuracy score after 5 runs is",np.std(test_acc_array))
+        print("weighted F1 array",weighted_F1_array)
+        print("test accuracy array",test_acc_array)
         
-        #optimizer = optim.Adam(model.parameters(), lr=0.001)
-        optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, alpha=0.9,weight_decay=0.0005, momentum=0.9)
-        optimizer.zero_grad()
-        
-        #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
-        path = '/data/sawasthi/data/Lara_motionminer/trainData_10/'
-        #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
-        #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
-        #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
-        train_dataset = CustomDataSet(path)
-        dataLoader_train = DataLoader(train_dataset, shuffle=True,
-                                      batch_size=batch_size,
-                                       num_workers=0,
-                                       pin_memory=True,
-                                       drop_last=True)
-      
-       
-        # Validation data    
-        path = '/data/sawasthi/data/Lara_motionminer/validationData_10/'
-        #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
-        #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
-        #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
-        validation_dataset = CustomDataSet(path)
-        dataLoader_validation = DataLoader(validation_dataset, shuffle=False,
-                                      batch_size=batch_size,
-                                       num_workers=0,
-                                       pin_memory=True,
-                                       drop_last=True)
-        
-        # Test data    
-        path = '/data/sawasthi/data/Lara_motionminer/testData_10/'
-        #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
-        #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
-        test_dataset = CustomDataSet(path)
-        dataLoader_test = DataLoader(test_dataset, shuffle=False,
-                                      batch_size=batch_size,
-                                       num_workers=0,
-                                       pin_memory=True,
-                                       drop_last=True)
-        '''
-        for b, harwindow_batched in enumerate(dataLoader_test):
-            data_x = harwindow_batched["data"]
-            data_x.to(device)
-            value = max_min_values(data_x,value)
-        '''
-        model_path_tl = '/data/sawasthi/data/Lara_motionminer/model/model_tl_JHMDB_acc_c1.pth'
-        training(dataLoader_train, dataLoader_validation,device)
-        WF, TA = testing(config)
-        #with open('S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/result.csv', 'w', newline='') as myfile:
-        #with open('S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/result.csv', 'w', newline='') as myfile:
-        weighted_F1_array.append(WF)
-        test_acc_array.append(TA)
-        
-    print("Mean Weighted F1 score after 5 runs is",np.mean(weighted_F1_array))
-    print("Standard deviation of Weighted F1 score after 5 runs is",np.std(weighted_F1_array))
-    
-    print("Mean Test accuracy score after 5 runs is",np.mean(test_acc_array))
-    print("Standard deviation of Test accuracy score after 5 runs is",np.std(test_acc_array))
-    print("weighted F1 array",weighted_F1_array)
-    print("test accuracy array",test_acc_array)
-    
