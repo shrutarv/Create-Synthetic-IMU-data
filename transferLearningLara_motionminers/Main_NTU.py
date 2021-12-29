adb03ab72bd0df8fc99d1a147c334b8982e7ee04
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-12-29 13:01:50 +0100

    Update Main_NTU.py

--- transferLearningLara_motionminers/Main_NTU.py
+++ transferLearningLara_motionminers/Main_NTU.py
@@ -545,7 +545,7 @@ if __name__ == '__main__':
                                            drop_last=True)
             
             # Test data    
-            path = '/data/sawasthi/data/dLara_motionminer/testData_10/'
+            path = '/data/sawasthi/data/Lara_motionminer/testData_10/'
             #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
             #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
             test_dataset = CustomDataSet(path)
