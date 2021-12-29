c8cb303784b80d6f727e68b08eef844b4e8c0aad
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2021-12-29 18:50:04 +0100

    Update Main.py

--- NTU/Main.py
+++ NTU/Main.py
@@ -627,7 +627,7 @@ if __name__ == '__main__':
         
         #model_path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/model.pth'
         #model_path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/'
-        path = '/data2/sawasthi/NTU/trainData_up_1a/'
+        path = '/data/sawasthi/NTU/trainData_up_1a/'
         #path = 'S:/Datasets/nturgbd_skeletons_s001_to_s017/train/'
         #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Train_data/"
@@ -641,7 +641,7 @@ if __name__ == '__main__':
        
         # Validation data    
         #path = 'S:/Datasets/nturgbd_skeletons_s001_to_s017/val/'
-        path = '/data2/sawasthi/NTU/validationData_up_1a/'
+        path = '/data/sawasthi/NTU/validationData_up_1a/'
         #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
         validation_dataset = CustomDataSet(path)
@@ -660,7 +660,7 @@ if __name__ == '__main__':
         '''
         training(dataLoader_train, dataLoader_validation,device,config,flag)
         print("Calculating accuracy for the trained model on validation set ")
-        path = '/data2/sawasthi/NTU/validationData_up_1a/'
+        path = '/data/sawasthi/NTU/validationData_up_1a/'
         #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
         #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
@@ -672,7 +672,7 @@ if __name__ == '__main__':
                                        drop_last=True)
         
         #testing(config)
-        path = '/data2/sawasthi/NTU/testData_up_1a/'
+        path = '/data/sawasthi/NTU/testData_up_1a/'
         #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/Test_pkl/'
         #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
         test_dataset = CustomDataSet(path)
@@ -754,7 +754,7 @@ if __name__ == '__main__':
       
        
         # Validation data    
-        path = '/data2/sawasthi/NTU/validationData_pose/'
+        path = '/data/sawasthi/NTU/validationData_pose/'
         validation_dataset = CustomDataSet(path)
         dataLoader_validation = DataLoader(validation_dataset, shuffle=False,
                                       batch_size=batch_size,
@@ -765,7 +765,7 @@ if __name__ == '__main__':
        
         training(dataLoader_train, dataLoader_validation,device,config,flag)
         print("Calculating accuracy for the trained model on validation set ")
-        path = '/data2/sawasthi/NTU/validationData_pose/'
+        path = '/data/sawasthi/NTU/validationData_pose/'
         test_dataset = CustomDataSet(path)
         dataLoader_test = DataLoader(validation_dataset, shuffle=False,
                                       batch_size=batch_size,
@@ -774,7 +774,7 @@ if __name__ == '__main__':
                                        drop_last=True)
         
         #testing(config)
-        path = '/data2/sawasthi/NTU/testData_pose/'
+        path = '/data/sawasthi/NTU/testData_pose/'
         test_dataset = CustomDataSet(path)
         dataLoader_test = DataLoader(test_dataset, shuffle=False,
                                       batch_size=batch_size,
