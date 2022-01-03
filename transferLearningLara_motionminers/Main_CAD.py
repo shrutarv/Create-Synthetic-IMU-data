e08663b0f6063a7d7f11dab0e283ef806e85423f
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-03 15:20:08 +0100

    Update Main_CAD.py

--- transferLearningLara_motionminers/Main_CAD.py
+++ transferLearningLara_motionminers/Main_CAD.py
@@ -446,7 +446,7 @@ if __name__ == '__main__':
     random.seed(seed)
 
     if torch.cuda.is_available():  
-          dev = "cuda:2" 
+          dev = "cuda:0" 
     else:  
           dev = "cpu"  
           
