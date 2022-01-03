9d10cf4ae3c940da2094926001fc433f2a3ddfea
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-03 11:31:31 +0100

    Update Main.py

--- Lara_motionminer/Main.py
+++ Lara_motionminer/Main.py
@@ -372,7 +372,7 @@ if __name__ == '__main__':
     total_loss = 0.0
     total_correct = 0
     epochs = 40
-    batch_size = 700
+    batch_size = 200
    
     flag = True
     iterations = 1
@@ -406,7 +406,7 @@ if __name__ == '__main__':
         #scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, patience=5)
         
         #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
-        model_path = '/data/sawasthi/Lara_motionminer/model/Laramm_model_10.pth'
+        model_path = '/data/sawasthi/Lara_motionminer/model/Laramm_model_cnn_10.pth'
         #model_path = 'S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/model.pth'
         path = '/data/sawasthi/Lara_motionminer/trainData_10/'
         #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows2/'
