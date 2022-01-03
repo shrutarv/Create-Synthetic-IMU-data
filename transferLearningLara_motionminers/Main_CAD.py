83aad82986e4908f6537c1638eade1e11f8a5fa7
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-03 21:37:01 +0100

    Update Main_CAD.py

--- transferLearningLara_motionminers/Main_CAD.py
+++ transferLearningLara_motionminers/Main_CAD.py
@@ -556,7 +556,7 @@ if __name__ == '__main__':
             value = max_min_values(data_x,value)
         '''
         model_path_tl = '/data/sawasthi/Lara_motionminer/model/model_tl_CAD_acc_c4.pth'
-        #training(dataLoader_train, dataLoader_validation,device,flag)
+        training(dataLoader_train, dataLoader_validation,device,flag)
         WF, TA = testing(config)
         flag = False
         #with open('S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/result.csv', 'w', newline='') as myfile:
