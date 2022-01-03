606fbebae551cd60d253583e0db1e707e6e778f0
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-03 21:36:25 +0100

    Update Main_CAD.py

--- transferLearningLara_motionminers/Main_CAD.py
+++ transferLearningLara_motionminers/Main_CAD.py
@@ -239,7 +239,8 @@ def set_required_grad(network):
         logging.info('        Network_User:        Setting Required_grad to Weights')
 
         if config["network"] == 'cnn':
-            list_layers = ['conv1_1.weight', 'conv1_1.bias'
+            list_layers = ['conv1_1.weight', 'conv1_1.bias','conv1_2.weight', 'conv1_2.bias',
+                           'conv2_1.weight', 'conv2_1.bias','conv2_2.weight', 'conv2_2.bias'
                            
                            ]
         elif config["network"] == 'cnn_imu':
@@ -274,7 +275,8 @@ def load_weights(network):
         #    print(k)
 
         if config["network"] == 'cnn':
-            list_layers = ['conv1_1.weight', 'conv1_1.bias'
+            list_layers = ['conv1_1.weight', 'conv1_1.bias','conv1_2.weight', 'conv1_2.bias',
+                           'conv2_1.weight', 'conv2_1.bias','conv2_2.weight', 'conv2_2.bias'
                            
                            ]
         elif config["network"] == 'cnn_imu':
@@ -478,10 +480,10 @@ if __name__ == '__main__':
     ws=100
     accumulation_steps = 5
     epochs = 30
-    batch_size = 300
+    batch_size = 100
     learning_rate = 0.00001
     print("epoch: ",epochs,"batch_size: ",batch_size,"accumulation steps: ",accumulation_steps,"ws: ",ws, "learning_rate: ",learning_rate)
-    iterations = 2
+    iterations = 3
     weighted_F1_array = []
     test_acc_array = []
     flag = True
@@ -553,7 +555,7 @@ if __name__ == '__main__':
             data_x.to(device)
             value = max_min_values(data_x,value)
         '''
-        model_path_tl = '/data/sawasthi/Lara_motionminer/model/model_tl_CAD_acc_c1.pth'
+        model_path_tl = '/data/sawasthi/Lara_motionminer/model/model_tl_CAD_acc_c4.pth'
         #training(dataLoader_train, dataLoader_validation,device,flag)
         WF, TA = testing(config)
         flag = False
