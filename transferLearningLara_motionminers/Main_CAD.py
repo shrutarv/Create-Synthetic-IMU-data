5865ad33d6e38ae2b148a3095a837ed83a731f1d
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-04 10:37:13 +0100

    Update Main_CAD.py

--- transferLearningLara_motionminers/Main_CAD.py
+++ transferLearningLara_motionminers/Main_CAD.py
@@ -527,7 +527,7 @@ if __name__ == '__main__':
                 path = '/data/sawasthi/Lara_motionminer/trainData_10_50/'
                 divide = 80
             elif prop==3:
-                path = '/data/sawasthi/Lara_motionminer/trainData_10_75/'
+                path = '/data/sawasthi/data/Lara_motionminer/trainData_10_75/'
                 divide = 100
             elif prop==4:
                 path = '/data/sawasthi/Lara_motionminer/trainData_10/' 
@@ -544,7 +544,7 @@ if __name__ == '__main__':
           
            
             # Validation data    
-            path = '/data/sawasthi/Lara_motionminer/validationData_10/'
+            path = '/data/sawasthi/data/Lara_motionminer/validationData_10/'
             #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
             #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
             #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
@@ -556,7 +556,7 @@ if __name__ == '__main__':
                                            drop_last=True)
             
             # Test data    
-            path = '/data/sawasthi/Lara_motionminer/testData_10/'
+            path = '/data/sawasthi/data/Lara_motionminer/testData_10/'
             #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
             #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
             test_dataset = CustomDataSet(path)
@@ -571,7 +571,7 @@ if __name__ == '__main__':
                 data_x.to(device)
                 value = max_min_values(data_x,value)
             '''
-            path_tl = '/data/sawasthi/LaraIMU/model/model_ntu_laraIMU_acc_cnn_c1'+str(prop)+'.pth'
+            path_tl = '/data/sawasthi/Lara_motionminer/model/model_ntu_laraIMU_acc_cnn_c1'+str(prop)+'.pth'
             model_path_tl = path_tl
             training(dataLoader_train, dataLoader_validation,device,flag)
             WF, TA = testing(config)
