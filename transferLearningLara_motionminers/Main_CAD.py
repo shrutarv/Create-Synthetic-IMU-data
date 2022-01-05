3341da4f5bccbe22095edf1f16e4abfac547d774
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-05 20:52:24 +0100

    Update Main_CAD.py

--- transferLearningLara_motionminers/Main_CAD.py
+++ transferLearningLara_motionminers/Main_CAD.py
@@ -471,6 +471,7 @@ if __name__ == '__main__':
         "output":"softmax",
         "num_classes":8,
         "reshape_input":False,
+        "dataset" : 'LaraMM',
         "freeze":False,
         "folder_exp_base_fine_tuning": '/data/sawasthi/CAD60/model/model_pose_ci_tf.pth'
         #"folder_exp_base_fine_tuning": 'S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/model_full.pth'
