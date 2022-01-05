fc0ef43ec5fab20ac770338874c9dcfbdcfc115c
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-05 16:29:31 +0100

    c4

--- transferLearningLara_motionminers/Main_Penn.py
+++ transferLearningLara_motionminers/Main_Penn.py
@@ -472,7 +472,7 @@ if __name__ == '__main__':
         "sliding_window_length":100,
         "filter_size":5,
         "num_filters":64,
-        "network":"cnn_imu",
+        "network":"cnn",
         "output":"softmax",
         "num_classes":8,
         "reshape_input":False,
