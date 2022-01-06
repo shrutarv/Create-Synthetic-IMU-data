1291a4e1e8e49177fca56b3400bdb83be2440160
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-06 22:29:01 +0100

    Update Main.py

--- NTU/Main.py
+++ NTU/Main.py
@@ -597,7 +597,7 @@ if __name__ == '__main__':
         ws=30
         accumulation_steps = 5
         epochs = 60
-        batch_size = 500
+        batch_size = 300
         learning_rate = 0.00001
         print("Starting for step size",config["step_size"])
         print("epoch: ",epochs,"batch_size: ", batch_size,"accumulation steps: ",accumulation_steps,"ws: ",ws, "learning_rate: ",learning_rate)
