c21e089650442d10b1b8cb62c547726858c16923
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-07 08:35:28 +0100

    Update Main_CAD.py

--- transferLearningLara_motionminers/Main_CAD.py
+++ transferLearningLara_motionminers/Main_CAD.py
@@ -280,14 +280,14 @@ def load_weights(network):
                            
                            ]
         elif config["network"] == 'cnn_imu':
-            list_layers = ['conv_LA_1_1.weight', 'conv_LA_1_1.bias',
-                           
+            list_layers = ['conv_LA_1_1.weight', 'conv_LA_1_1.bias', 'conv_LA_1_2.weight', 'conv_LA_1_2.bias',
+                           'conv_LA_2_1.weight', 'conv_LA_2_1.bias', 'conv_LA_2_2.weight', 'conv_LA_2_2.bias',
                            #'conv_LL_1_1.weight', 'conv_LL_1_1.bias', 'conv_LL_1_2.weight', 'conv_LL_1_2.bias',
                            #'conv_LL_2_1.weight', 'conv_LL_2_1.bias', 'conv_LL_2_2.weight', 'conv_LL_2_2.bias',
-                           'conv_N_1_1.weight', 'conv_N_1_1.bias', 
-                           
-                           'conv_RA_1_1.weight', 'conv_RA_1_1.bias'
-                           
+                           'conv_N_1_1.weight', 'conv_N_1_1.bias', 'conv_N_1_2.weight', 'conv_N_1_2.bias',
+                           'conv_N_2_1.weight', 'conv_N_2_1.bias', 'conv_N_2_2.weight', 'conv_N_2_2.bias',
+                           'conv_RA_1_1.weight', 'conv_RA_1_1.bias', 'conv_RA_1_2.weight', 'conv_RA_1_2.bias',
+                           'conv_RA_2_1.weight', 'conv_RA_2_1.bias', 'conv_RA_2_2.weight', 'conv_RA_2_2.bias',
                            #'conv_RL_1_1.weight', 'conv_RL_1_1.bias', 'conv_RL_1_2.weight', 'conv_RL_1_2.bias',
                            #'conv_RL_2_1.weight', 'conv_RL_2_1.bias', 'conv_RL_2_2.weight', 'conv_RL_2_2.bias'
                            ]
@@ -479,7 +479,7 @@ if __name__ == '__main__':
         }
 
 
-    proportions_opts = [0,1,2,4]
+    proportions_opts = [4]
     flag = True
     iterations = 3
     
@@ -535,7 +535,7 @@ if __name__ == '__main__':
                 path = '/data/sawasthi/Lara_motionminer/trainData_10_75/'
                 divide = 100
             elif prop==4:
-                path = '/data/sawasthi/Lara_motionminer/trainData_10/' 
+                path = '/data/sawasthi/data/Lara_motionminer/trainData_10/' 
                 divide = 100
              #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
             #path = 'S:/MS A&R/4th Sem/Thesis/PAMAP2_Dataset/pkl files'
@@ -549,7 +549,7 @@ if __name__ == '__main__':
           
            
             # Validation data    
-            path = '/data/sawasthi/Lara_motionminer/validationData_10/'
+            path = '/data/sawasthi/data/Lara_motionminer/validationData_10/'
             #path = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
             #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
             #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
@@ -561,7 +561,7 @@ if __name__ == '__main__':
                                            drop_last=True)
             
             # Test data    
-            path = '/data/sawasthi/Lara_motionminer/testData_10/'
+            path = '/data/sawasthi/data/Lara_motionminer/testData_10/'
             #path = 'S:/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows/'
             #path = "S:/MS A&R/4th Sem/Thesis/LaRa/OMoCap data/Test_data/"
             test_dataset = CustomDataSet(path)
@@ -576,7 +576,7 @@ if __name__ == '__main__':
                 data_x.to(device)
                 value = max_min_values(data_x,value)
             '''
-            path_tl = '/data/sawasthi/Lara_motionminer/model/model_cad_laraMM_pose_ci_c1'+str(prop)+'.pth'
+            path_tl = '/data/sawasthi/data/Lara_motionminer/model/model_cad_laraMM_pose_ci_c4'+str(prop)+'.pth'
             model_path_tl = path_tl
             training(dataLoader_train, dataLoader_validation,device,flag)
             WF, TA = testing(config)
