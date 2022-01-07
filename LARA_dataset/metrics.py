ad552994ddab798fdc08f17dae2d1ffbc315c8c7
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-07 18:34:07 +0100

    Update metrics.py

--- LARA_dataset/metrics.py
+++ LARA_dataset/metrics.py
@@ -28,7 +28,8 @@ class Metrics(object):
             self.attr[attr_idx, 1:] = self.attr[attr_idx, 1:] / np.linalg.norm(self.attr[attr_idx, 1:])
 
         self.atts = torch.from_numpy(self.attr).type(dtype=torch.FloatTensor)
-        self.atts = self.atts.type(dtype=torch.cuda.FloatTensor)
+        self.atts = self.atts.type(dtype=torch.FloatTensor)
+        #self.atts = self.atts.type(dtype=torch.cuda.FloatTensor)
         self.results = {'acc': 0, 'f1_weighted': 0, 'f1_mean': 0, 'predicted_classes': 0, 'precision': 0,
                         'recall': 0}
 
