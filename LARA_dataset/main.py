5f822b3aab4b3173de7bb089f765a665fd783ea3
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-09 00:02:15 +0100

    Update main.py

--- LARA_dataset/main.py
+++ LARA_dataset/main.py
@@ -331,8 +331,8 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                     'motionminers_flw': "path_to_datasets_folder/" + 'motionminers_flw/'}
 
     # GPU
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-    GPU = 1
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    GPU = 2
 
     # Labels position on the segmented window
     label_pos = {0: 'middle', 1: 'mode', 2: 'end'}
