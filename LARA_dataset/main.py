9bc76d5d387a3d1e8257a3866de4e236e2953787
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-09 20:25:52 +0100

    Update main.py

--- LARA_dataset/main.py
+++ LARA_dataset/main.py
@@ -440,7 +440,7 @@ def main():
     for more information about all of possible configurations for the experiments
 
     """
-    dataset_idx = [12]
+    dataset_idx = [1]
     network_idx = [0]
     reshape_input = [False]
     output_idxs = [0]
@@ -462,7 +462,7 @@ def main():
                                         config = configuration(dataset_idx=dataset_idx[dts],
                                                                network_idx=network_idx[nt],
                                                                output_idx=opt,
-                                                               usage_modus_idx=0,
+                                                               usage_modus_idx=5,
                                                                dataset_fine_tuning_idx=dft,
                                                                reshape_input=reshape_input[rsi],
                                                                learning_rates_idx=lr,
