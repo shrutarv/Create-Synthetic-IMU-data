8e4dc0ffaccac326c6479d2c2d12c193018e8614
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-10 12:21:19 +0100

    c4

--- LARA_dataset/main.py
+++ LARA_dataset/main.py
@@ -299,7 +299,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                                       network[network_idx] + '/' + output[output_idx] + '/' + fully_convolutional + \
                                       '/' + reshape_folder + '/' + 'final/'
     elif usage_modus[usage_modus_idx] == 'fine_tuning':
-       folder_exp = '/data/sawasthi/mbientlab/model/jhmdb_LaraIMU_acc_ci_c1' + str(per_data) 
+       folder_exp = '/data/sawasthi/mbientlab/model/jhmdb_LaraIMU_acc_ci_c4' + str(per_data) 
        folder_exp_base_fine_tuning = '/data/sawasthi/JHMDB/model/model_acc_ci_up4_tf.pth' #model_acc_up4.pth #model_up1_3a.pt
        
        
