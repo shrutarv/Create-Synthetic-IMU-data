5f123ec6a3d12e5f5fb0e17bd6cc6a9864046ae1
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-11 09:17:21 +0100

    Update main.py

--- LARA_dataset/main.py
+++ LARA_dataset/main.py
@@ -273,8 +273,8 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
 
     # Folder
     if usage_modus[usage_modus_idx] == 'train':
-       folder_exp = '/data/sawasthi/LaraMM/model/'
-       folder_exp_base_fine_tuning = '/data/sawasthi/LaraMM/model/network_LaraMM_100.pth' #model_acc_up4.pth #model_up1_3a.pt
+       folder_exp = '/data/sawasthi/mbientlab/model/'
+       folder_exp_base_fine_tuning = '/data/sawasthi/mbientlab/model/network_LaraIMU_ci_100.pth' #model_acc_up4.pth #model_up1_3a.pt
        '''
         
        folder_exp = folder_base + dataset[dataset_idx] + '/' + \
@@ -448,7 +448,7 @@ def main():
     dataset_ft_idx = [0]
     counter_exp = 0
     freeze = [0]
-    proport = [0,1,2,3,4]
+    proport = [4]
     percentages = [12]
     for dts in range(len(dataset_idx)):
         for nt in range(len(network_idx)):
@@ -462,7 +462,7 @@ def main():
                                         config = configuration(dataset_idx=dataset_idx[dts],
                                                                network_idx=network_idx[nt],
                                                                output_idx=opt,
-                                                               usage_modus_idx=5,
+                                                               usage_modus_idx=0,
                                                                dataset_fine_tuning_idx=dft,
                                                                reshape_input=reshape_input[rsi],
                                                                learning_rates_idx=lr,
