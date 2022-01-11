c6a9098c3c5f90bfa415997449965398d138890a
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-11 09:24:20 +0100

    Update modus_selecter.py

--- LARA_dataset/modus_selecter.py
+++ LARA_dataset/modus_selecter.py
@@ -219,7 +219,7 @@ class Modus_Selecter(object):
         """
         logging.info('    Network_selecter: Net modus: {}'.format(self.config['usage_modus']))
         if self.config['usage_modus'] == 'train':
-            self.train(itera=3, testing=True)
+            self.train(itera=4, testing=True)
         elif self.config['usage_modus'] == 'test':
             self.test()
         elif self.config['usage_modus'] == 'evolution':
