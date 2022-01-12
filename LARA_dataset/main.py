7e2864947ce501c3858165d60696df2b536d3093
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-01-12 22:03:22 +0100

    Update main.py

--- LARA_dataset/main.py
+++ LARA_dataset/main.py
@@ -299,8 +299,8 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                                       network[network_idx] + '/' + output[output_idx] + '/' + fully_convolutional + \
                                       '/' + reshape_folder + '/' + 'final/'
     elif usage_modus[usage_modus_idx] == 'fine_tuning':
-       folder_exp = '/data/sawasthi/mbientlab/model/penn_LaraMM_acc_cnn_c1' + str(per_data) 
-       folder_exp_base_fine_tuning = '/data/sawasthi/Penn/model/model_acc_up2_tf.pth' #model_acc_up4.pth #model_up1_3a.pt
+       folder_exp = '/data/sawasthi/mbientlab/model/cad_LaraIMU_acc_ci_c1' + str(per_data) 
+       folder_exp_base_fine_tuning = '/data/sawasthi/CAD60/model/model_acc_ci_up3_tf.pth' #model_acc_up4.pth #model_up1_3a.pt
        
        
        
@@ -440,15 +440,15 @@ def main():
     for more information about all of possible configurations for the experiments
 
     """
-    dataset_idx = [12]
-    network_idx = [0]
+    dataset_idx = [1]
+    network_idx = [2]
     reshape_input = [False]
     output_idxs = [0]
     lrs = [0]
     dataset_ft_idx = [0]
     counter_exp = 0
     freeze = [0]
-    proport = [0,1,2,4]
+    proport = [4]
     percentages = [12]
     for dts in range(len(dataset_idx)):
         for nt in range(len(network_idx)):
