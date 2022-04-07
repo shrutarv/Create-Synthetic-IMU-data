cae5d4c1d0381b8eabf064c0aedd5aa1074435da
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-04-07 14:24:04 +0200

    Update op_sliding_window_acc.py

--- JHMDB/op_sliding_window_acc.py
+++ JHMDB/op_sliding_window_acc.py
@@ -206,17 +206,17 @@ if __name__ == '__main__':
     # The training, test and validation data have been separately interpolated and 
     # up sampled
     # up sampling rate
-    up = 1
-    ws = (50,30)
+    up = 4
+    ws = (200,30)
     #ws = (25,30) 
     ss = (12,30)     
     #ss = (25,31)
-    sliding_window_length = 50  
+    sliding_window_length = 200  
     #sliding_window_length = 100    
     sliding_window_step = 12
     
     df = pd.read_csv('/home/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/train_data.csv')
-    #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/train_data.csv')
+    #df = pd.read_csv('S:/GitHub/Transfer_Learning_HAR/Create-Synthetic-IMU-data/JHMDB/train_data.csv')
     data = df.values
     data_new = data[:,1:31]
     attr = np.zeros((100,1))
@@ -285,8 +285,8 @@ if __name__ == '__main__':
     
     #data_dir = "/media/shrutarv/Drive1/MS A&R/4th Sem/Thesis/LaRa/IMU data/IMU data/Windows2/"
     #df = pd.read_csv('S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/train_data25_39.csv')
-    data_dir =  '/data/sawasthi/JHMDB/trainData_acc_up1_2s/'
-    #data_dir = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl/'
+    data_dir =  '/data/sawasthi/JHMDB/trainData_acc_up4_2s/'
+    #data_dir = 'S:/Datasets/JHMDB/train/'
     label = np.repeat(data[:,31],up).astype(int)
     lab = np.zeros((len(label),20), dtype=int)
     lab[:,0] = label
@@ -296,9 +296,11 @@ if __name__ == '__main__':
     example_creating_windows_file(k, X, lab, data_dir)
     print("train data pickled")
     
-    #data_dir = 'S:/MS A&R/4th Sem/Thesis/J-HMDB/joint_positions/train/pkl'
-    data_dir =  '/data/sawasthi/JHMDB/testData_acc_up1_2s/'
+    #data_dir = 'S:/Datasets/JHMDB/test/'
+    data_dir =  '/data/sawasthi/JHMDB/testData_acc_up4_2s/'
     df = pd.read_csv('/home/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/test_data.csv')
+    #df = pd.read_csv('S:/GitHub/Transfer_Learning_HAR/Create-Synthetic-IMU-data/JHMDB/test_data.csv')
+    
     data = df.values
     data = normalize(data,value, "test")
     print("test data normalized")
@@ -329,9 +331,11 @@ if __name__ == '__main__':
     example_creating_windows_file(k, X, lab, data_dir)
     print("test data pickled")
     
-    data_dir =  '/data/sawasthi/JHMDB/validationData_acc_up1_2s/'
-    #data_dir =  '/data/sawasthi/data/JHMDB/validationData/'
-    df = pd.read_csv('/home/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/validation_data.csv')
+    data_dir =  '/data/sawasthi/JHMDB/validationData_acc_up4_2s/'
+    #data_dir =  'S:/Datasets/JHMDB/val/'
+    #df = pd.read_csv('/home/sawasthi/Thesis--Create-Synthetic-IMU-data/JHMDB/validation_data.csv')
+    df = pd.read_csv('S:/GitHub/Transfer_Learning_HAR/Create-Synthetic-IMU-data/JHMDB/validation_data.csv')
+    
     data = df.values
     data = normalize(data,value, "validation")
     print("validation data normalized")
