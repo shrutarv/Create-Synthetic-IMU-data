571ef2f50b0ef11c90d778abb7b6fec5b193a432
shrutarv awasthi <shrutarv.awasthi@tu-dortmund.de>
2022-04-15 16:52:06 +0200

    cleaning

--- BerkleyMHAD/pre_processing.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 22 20:56:15 2020
-
-@author: STUDENT
-"""
-from bvh import Bvh
-with open('S:/MS A&R/4th Sem/Thesis/Berkley MHAD/SkeletalData-20200922T160342Z-001/SkeletalData/skl_s01_a01_r01.bvh') as f:
-    mocap = Bvh(f.read())
--- __init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  8 21:15:23 2021
-
-@author: STUDENT
-"""
